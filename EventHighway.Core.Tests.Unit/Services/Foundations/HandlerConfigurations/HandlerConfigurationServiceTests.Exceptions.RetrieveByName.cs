48d6d9530fde05ae1106502bf972a61ca23653d9
Christo du Toit <christo@dutoit.co.uk>
2026-04-20 23:12:54 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveByNameIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/HandlerConfigurations/HandlerConfigurationServiceTests.Exceptions.RetrieveByName.cs
@@ -0,0 +1,64 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.HandlerConfigurations;
+using EventHighway.Core.Models.Services.Foundations.HandlerConfigurations.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.HandlerConfigurations
+{
+    public partial class HandlerConfigurationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByNameIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            string someHandlerConfigurationName = GetRandomString();
+            SqlException sqlException = GetSqlException();
+
+            var failedHandlerConfigurationStorageException =
+                new FailedHandlerConfigurationStorageException(
+                    message: "Failed handler configuration storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedHandlerConfigurationDependencyException =
+                new HandlerConfigurationDependencyException(
+                    message: "Handler configuration dependency error occurred, contact support.",
+                    innerException: failedHandlerConfigurationStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllHandlerConfigurationsAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<HandlerConfiguration> retrieveHandlerConfigurationByNameTask =
+                this.handlerConfigurationService.RetrieveHandlerConfigurationByNameAsync(
+                    someHandlerConfigurationName);
+
+            HandlerConfigurationDependencyException actualHandlerConfigurationDependencyException =
+                await Assert.ThrowsAsync<HandlerConfigurationDependencyException>(
+                    retrieveHandlerConfigurationByNameTask.AsTask);
+
+            // then
+            actualHandlerConfigurationDependencyException.Should()
+                .BeEquivalentTo(expectedHandlerConfigurationDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllHandlerConfigurationsAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedHandlerConfigurationDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
