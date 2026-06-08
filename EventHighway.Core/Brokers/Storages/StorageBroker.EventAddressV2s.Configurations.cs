ccf32d11cd5bca4a2a8714fb002bc887e51bb6dd
Christo du Toit <christo@dutoit.co.uk>
2026-06-08 20:40:47 +0100

    DATA: EventListenerV2 Unique Constraint

--- EventHighway.Core/Brokers/Storages/StorageBroker.EventAddressV2s.Configurations.cs
+++ EventHighway.Core/Brokers/Storages/StorageBroker.EventAddressV2s.Configurations.cs
@@ -14,6 +14,9 @@ namespace EventHighway.Core.Brokers.Storages
         {
             model.ToTable("EventAddressV2s");
             model.HasKey(eventAddressV2 => eventAddressV2.Id);
+
+            model.HasIndex(eventAddressV2 => eventAddressV2.Name)
+                .IsUnique();
         }
     }
 }
