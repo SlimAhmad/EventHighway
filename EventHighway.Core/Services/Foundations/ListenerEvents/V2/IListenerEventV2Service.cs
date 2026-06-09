dabd6b608b96a7762fc6805e39ad4724d0010fa7
Christo du Toit <christo@dutoit.co.uk>
2026-06-09 17:37:43 +0100

    CODE RUB: Code Cleanup

--- EventHighway.Core/Services/Foundations/ListenerEvents/V2/IListenerEventV2Service.cs
+++ EventHighway.Core/Services/Foundations/ListenerEvents/V2/IListenerEventV2Service.cs
@@ -12,9 +12,18 @@ namespace EventHighway.Core.Services.Foundations.ListenerEvents.V2
 {
     internal interface IListenerEventV2Service
     {
-        ValueTask<ListenerEventV2> AddListenerEventV2Async(ListenerEventV2 listenerEventV2, CancellationToken cancellationToken = default);
+        ValueTask<ListenerEventV2> AddListenerEventV2Async(
+            ListenerEventV2 listenerEventV2,
+            CancellationToken cancellationToken = default);
+
         ValueTask<IQueryable<ListenerEventV2>> RetrieveAllListenerEventV2sAsync();
-        ValueTask<ListenerEventV2> ModifyListenerEventV2Async(ListenerEventV2 listenerEventV2, CancellationToken cancellationToken = default);
-        ValueTask<ListenerEventV2> RemoveListenerEventV2ByIdAsync(Guid listenerEventV2Id, CancellationToken cancellationToken = default);
+
+        ValueTask<ListenerEventV2> ModifyListenerEventV2Async(
+            ListenerEventV2 listenerEventV2,
+            CancellationToken cancellationToken = default);
+
+        ValueTask<ListenerEventV2> RemoveListenerEventV2ByIdAsync(
+            Guid listenerEventV2Id,
+            CancellationToken cancellationToken = default);
     }
 }
