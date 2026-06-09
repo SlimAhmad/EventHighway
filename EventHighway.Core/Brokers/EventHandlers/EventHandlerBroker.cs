a6478c2eaf54c4dd7ea9de78f7e5a0512b744af7
Christo du Toit <christo@dutoit.co.uk>
2026-06-09 13:48:32 +0100

    BROKERS: EventHandlerBroker

---
+++ EventHighway.Core/Brokers/EventHandlers/EventHandlerBroker.cs
@@ -0,0 +1,11 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using EventHighway.Abstractions.EventHandlers;
+
+namespace EventHighway.Core.Brokers.EventHandlers
+{
+    internal interface IEventHandlerBroker : IEventHandler
+    { }
+}
