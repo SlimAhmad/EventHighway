3fae980d6c4b8dbc2f80abbc16f9c1a057b94931
Christo du Toit <christo@dutoit.co.uk>
2026-06-10 17:44:05 +0100

    ShouldThrowDependencyValidationExceptionOnPostWithSecretIfHttpUnprocessableErrorOccursAndLogItAsync -> PASS

--- EventHighway.EventHandlers.Tests.Unit/Services/Rest/RestServiceTests.Exceptions.PostWithSecret.cs
+++ EventHighway.EventHandlers.Tests.Unit/Services/Rest/RestServiceTests.Exceptions.PostWithSecret.cs
@@ -2,11 +2,13 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using EventHighway.Abstractions.EventHandlers;
 using EventHighway.EventHandlers.Models.Foundations.Rest.Exceptions;
 using FluentAssertions;
 using Moq;
+using RESTFulSense.Exceptions;
 using Xeptions;
 
 namespace EventHighway.EventHandlers.Tests.Unit.Services.Rest
@@ -58,6 +60,67 @@ namespace EventHighway.EventHandlers.Tests.Unit.Services.Rest
             // then
             actualRestServiceDependencyException.Should()
                 .BeEquivalentTo(expectedRestServiceDependencyException);
+
+            this.apiBrokerMock.Verify(broker =>
+                broker.PostWithSecretAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>()),
+                        Times.Once);
+
+            this.apiBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task
+            ShouldThrowDependencyValidationExceptionOnPostWithSecretIfHttpUnprocessableErrorOccursAndLogItAsync()
+        {
+            // given
+            string randomContent = GetRandomString();
+
+            var someHandlerParams = CreateSecretHandlerParams(
+                url: GetRandomString(),
+                secret: GetRandomString());
+
+            var httpUnprocessableEntityException =
+                new HttpResponseUnprocessableEntityException();
+
+            httpUnprocessableEntityException.Data.Add(
+                "ErrorCode",
+                new List<string> { "UnprocessableEntity" });
+
+            var failedRequestRestServiceException =
+                new FailedRequestRestServiceException(
+                    message: "Failed rest service request error occurred, fix the errors and try again.",
+                    innerException: httpUnprocessableEntityException,
+                    data: httpUnprocessableEntityException.Data);
+
+            var expectedRestServiceDependencyValidationException =
+                new RestServiceDependencyValidationException(
+                    message: "Rest service dependency validation error occurred, fix the errors and try again.",
+                    innerException: failedRequestRestServiceException);
+
+            this.apiBrokerMock.Setup(broker =>
+                broker.PostWithSecretAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>()))
+                        .ThrowsAsync(httpUnprocessableEntityException);
+
+            // when
+            ValueTask<EventHandlerResult> postWithSecretTask =
+                this.restService.PostWithSecretAsync(
+                    content: randomContent,
+                    handlerParams: someHandlerParams,
+                    cancellationToken: TestContext.Current.CancellationToken);
+
+            RestServiceDependencyValidationException actualRestServiceDependencyValidationException =
+                await Assert.ThrowsAsync<RestServiceDependencyValidationException>(
+                    postWithSecretTask.AsTask);
+
+            // then
+            actualRestServiceDependencyValidationException.Should()
+                .BeEquivalentTo(expectedRestServiceDependencyValidationException);
 
             this.apiBrokerMock.Verify(broker =>
                 broker.PostWithSecretAsync(
