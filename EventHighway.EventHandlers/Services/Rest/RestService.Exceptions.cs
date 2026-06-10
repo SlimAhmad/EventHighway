089a9d5c054b0ea9d014d3f72bb6e1c5e12150f2
Christo du Toit <christo@dutoit.co.uk>
2026-06-10 17:36:18 +0100

    ShouldThrowCriticalDependencyExceptionOnPostWithSecretIfCriticalDependencyErrorOccursAndLogItAsync -> PASS

--- EventHighway.EventHandlers/Services/Rest/RestService.Exceptions.cs
+++ EventHighway.EventHandlers/Services/Rest/RestService.Exceptions.cs
@@ -5,6 +5,7 @@
 using System.Threading.Tasks;
 using EventHighway.Abstractions.EventHandlers;
 using EventHighway.EventHandlers.Models.Foundations.Rest.Exceptions;
+using RESTFulSense.Exceptions;
 using Xeptions;
 
 namespace EventHighway.EventHandlers.Servies.Rest
@@ -24,11 +25,40 @@ namespace EventHighway.EventHandlers.Servies.Rest
             {
                 throw CreateValidationException(invalidRestServiceException);
             }
+            catch (HttpResponseUrlNotFoundException httpResponseUrlNotFoundException)
+            {
+                throw CreateCriticalDependencyException(httpResponseUrlNotFoundException);
+            }
+            catch (HttpResponseUnauthorizedException httpResponseUnauthorizedException)
+            {
+                throw CreateCriticalDependencyException(httpResponseUnauthorizedException);
+            }
+            catch (HttpResponseForbiddenException httpResponseForbiddenException)
+            {
+                throw CreateCriticalDependencyException(httpResponseForbiddenException);
+            }
+            catch (HttpResponseMethodNotAllowedException httpResponseMethodNotAllowedException)
+            {
+                throw CreateCriticalDependencyException(httpResponseMethodNotAllowedException);
+            }
         }
 
         private static RestServiceValidationException CreateValidationException(Xeption exception) =>
             new RestServiceValidationException(
                 message: "Rest service validation error occurred, fix the errors and try again.",
                 innerException: exception);
+
+        private static RestServiceDependencyException CreateCriticalDependencyException(Xeption exception)
+        {
+            var failedRestServiceException =
+                new FailedRestServiceException(
+                    message: "Failed rest service error occurred, contact support.",
+                    innerException: exception,
+                    data: exception.Data);
+
+            return new RestServiceDependencyException(
+                message: "Rest service dependency error occurred, contact support.",
+                innerException: failedRestServiceException);
+        }
     }
 }
