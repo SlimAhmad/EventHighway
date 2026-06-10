d86a3f741af958bab839701dc82e3eff17e9c6e7
Christo du Toit <christo@dutoit.co.uk>
2026-06-11 00:33:03 +0100

    ShouldInvokeAsync -> FAIL

---
+++ EventHighway.EventHandlers/Services/Delegates/DelegateService.cs
@@ -0,0 +1,35 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Abstractions.EventHandlers;
+
+namespace EventHighway.EventHandlers.Services.Delegates
+{
+    internal partial class DelegateService : IDelegateService
+    {
+        private readonly Func<
+            string,
+            IReadOnlyDictionary<string, string>,
+            CancellationToken,
+            ValueTask<EventHandlerResult>> handler;
+
+        public DelegateService(
+            Func<string, IReadOnlyDictionary<string, string>, CancellationToken, ValueTask<EventHandlerResult>> handler)
+        {
+            this.handler = handler;
+        }
+
+        public virtual async ValueTask<EventHandlerResult> InvokeAsync(
+            string content,
+            IReadOnlyDictionary<string, string> handlerParams,
+            CancellationToken cancellationToken = default)
+        {
+            return null;
+        }
+    }
+}
