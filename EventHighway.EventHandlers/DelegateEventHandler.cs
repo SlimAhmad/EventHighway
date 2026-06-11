c528904eb9382b9c9bd2bc357a8a77626cba1c23
Christo du Toit <christo@dutoit.co.uk>
2026-06-11 01:14:29 +0100

    ShouldHandleWithDelegateAsync -> PASS

--- EventHighway.EventHandlers/DelegateEventHandler.cs
+++ EventHighway.EventHandlers/DelegateEventHandler.cs
@@ -35,7 +35,7 @@ namespace EventHighway.EventHandlers
         {
             try
             {
-                return await this.delegateService.InvokeAsync(content, null, cancellationToken);
+                return await this.delegateService.InvokeAsync(content, handlerParams, cancellationToken);
             }
             catch (DelegateServiceValidationException delegateServiceValidationException)
             {
