c46c5188267e977a333089584fd866fc7d0fc35c
Christo du Toit <christo@dutoit.co.uk>
2026-06-12 13:05:52 +0100

    IMPORTS: EventListenerV2sClient

---
+++ EventHighway.Core.Tests.Unit/Clients/EventListeners/V2/EventListenerV2sClientTests.Logic.Register.cs
@@ -0,0 +1,62 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventListeners.V2;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Clients.EventListeners.V2
+{
+    public partial class EventListenerV2sClientTests
+    {
+        [Fact]
+        public async Task ShouldRegisterEventListenerV2Async()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventListenerV2 randomEventListenerV2 =
+                CreateRandomEventListenerV2();
+
+            EventListenerV2 inputEventListenerV2 =
+                randomEventListenerV2;
+
+            EventListenerV2 registeredEventListenerV2 =
+                inputEventListenerV2;
+
+            EventListenerV2 expectedEventListenerV2 =
+                registeredEventListenerV2.DeepClone();
+
+            this.eventListenerV2OrchestrationServiceMock.Setup(service =>
+                service.AddEventListenerV2Async(
+                    inputEventListenerV2,
+                    randomCancellationToken))
+                        .ReturnsAsync(registeredEventListenerV2);
+
+            // when
+            EventListenerV2 actualEventListenerV2 =
+                await this.eventListenerV2sClient
+                    .RegisterEventListenerV2Async(
+                        inputEventListenerV2,
+                        randomCancellationToken);
+
+            // then
+            actualEventListenerV2.Should()
+                .BeEquivalentTo(expectedEventListenerV2);
+
+            this.eventListenerV2OrchestrationServiceMock.Verify(service =>
+                service.AddEventListenerV2Async(
+                    inputEventListenerV2,
+                    randomCancellationToken),
+                        Times.Once);
+
+            this.eventListenerV2OrchestrationServiceMock
+                .VerifyNoOtherCalls();
+        }
+    }
+}
