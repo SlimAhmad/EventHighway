ff35bea58f9863994b73a84563d2be53554a74d0
Christo du Toit <christo@dutoit.co.uk>
2026-06-13 18:21:39 +0100

    ShouldThrowDependencyValidationExceptionOnRetrieveHealthSummaryIfValidationExceptionOccursAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Coordinations/HealthChecks/V2/HealthV2CoordinationServiceTests.Exceptions.RetrieveHealthSummary.cs
@@ -0,0 +1,110 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Coordinations.HealthChecks.V2;
+using EventHighway.Core.Models.Coordinations.HealthChecks.V2.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Coordinations.HealthChecks.V2
+{
+    public partial class HealthV2CoordinationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnRetrieveHealthSummaryIfValidationExceptionOccursAndLogItAsync(
+            Xeption validationException)
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            var expectedHealthV2CoordinationDependencyValidationException =
+                new HealthV2CoordinationDependencyValidationException(
+                    message: "Health validation error occurred, fix the errors and try again.",
+                    innerException: validationException.InnerException as Xeption);
+
+            this.eventV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllEventV2sAsync(It.IsAny<CancellationToken>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ValueTask<IEnumerable<HealthCheckItemV2>> retrieveTask =
+                this.healthV2CoordinationService
+                    .RetrieveHealthSummaryV2Async(randomCancellationToken);
+
+            HealthV2CoordinationDependencyValidationException actualException =
+                await Assert.ThrowsAsync<HealthV2CoordinationDependencyValidationException>(
+                    retrieveTask.AsTask);
+
+            // then
+            actualException.Should()
+                .BeEquivalentTo(expectedHealthV2CoordinationDependencyValidationException);
+
+            this.eventV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventV2sAsync(It.IsAny<CancellationToken>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedHealthV2CoordinationDependencyValidationException))),
+                        Times.Once);
+
+            this.eventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.eventListenerV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.eventArchiveV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveHealthSummaryIfDependencyExceptionOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            var expectedHealthV2CoordinationDependencyException =
+                new HealthV2CoordinationDependencyException(
+                    message: "Health dependency error occurred, contact support.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.eventV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllEventV2sAsync(It.IsAny<CancellationToken>()))
+                    .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask<IEnumerable<HealthCheckItemV2>> retrieveTask =
+                this.healthV2CoordinationService
+                    .RetrieveHealthSummaryV2Async(randomCancellationToken);
+
+            HealthV2CoordinationDependencyException actualException =
+                await Assert.ThrowsAsync<HealthV2CoordinationDependencyException>(
+                    retrieveTask.AsTask);
+
+            // then
+            actualException.Should()
+                .BeEquivalentTo(expectedHealthV2CoordinationDependencyException);
+
+            this.eventV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventV2sAsync(It.IsAny<CancellationToken>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedHealthV2CoordinationDependencyException))),
+                        Times.Once);
+
+            this.eventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.eventListenerV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.eventArchiveV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
