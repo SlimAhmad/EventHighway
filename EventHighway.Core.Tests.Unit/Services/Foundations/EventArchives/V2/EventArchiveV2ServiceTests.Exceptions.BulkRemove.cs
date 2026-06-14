ba4d1927ef38c3bc4991b4c83b70e496d4d7918f
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-06-14 15:45:36 +0100

    ShouldThrowCriticalDependencyExceptionOnBulkRemoveIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventArchives/V2/EventArchiveV2ServiceTests.Exceptions.BulkRemove.cs
@@ -0,0 +1,84 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V2;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V2.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventArchives.V2
+{
+    public partial class EventArchiveV2ServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnBulkRemoveIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            IQueryable<EventArchiveV2> randomEventArchiveV2s =
+                CreateRandomEventArchiveV2s();
+
+            List<EventArchiveV2> inputEventArchiveV2s =
+                randomEventArchiveV2s.ToList();
+
+            SqlException sqlException = CreateSqlException();
+            sqlException.Data.Add("ErrorCode", new List<string> { "SqlError" });
+
+            sqlException.Data.Add(
+                key: nameof(SqlException.Number),
+                value: new List<string> { "Some SQL error code" });
+
+            var failedStorageEventArchiveV2Exception =
+                new FailedStorageEventArchiveV2Exception(
+                    message: "Failed event archive storage error occurred, contact support.",
+                    innerException: sqlException,
+                    data: sqlException.Data);
+
+            var expectedEventArchiveV2DependencyException =
+                new EventArchiveV2DependencyException(
+                    message: "Event archive dependency error occurred, contact support.",
+                    innerException: failedStorageEventArchiveV2Exception);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.DeleteBulkEventArchiveV2sAsync(
+                    inputEventArchiveV2s,
+                    It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask bulkAddEventArchiveV2sTask =
+                this.eventArchiveV2Service.BulkRemoveEventArchiveV2sAsync(
+                    inputEventArchiveV2s,
+                    TestContext.Current.CancellationToken);
+
+            EventArchiveV2DependencyException actualEventArchiveV2DependencyException =
+                await Assert.ThrowsAsync<EventArchiveV2DependencyException>(
+                    bulkAddEventArchiveV2sTask.AsTask);
+
+            // then
+            actualEventArchiveV2DependencyException.Should()
+                .BeEquivalentTo(expectedEventArchiveV2DependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteBulkEventArchiveV2sAsync(
+                    inputEventArchiveV2s,
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedEventArchiveV2DependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
