c90c9b7d0ca37b1e0e3c0415b0e4fd6f5c216191
Christo du Toit <christo@dutoit.co.uk>
2026-06-15 15:34:31 +0100

    ShouldReturnNAForHandlerCountWhenNoThresholdIsConfiguredAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Coordinations/HealthChecks/V2/HealthV2CoordinationServiceTests.Logic.RetrieveHealthSummary.cs
+++ EventHighway.Core.Tests.Unit/Services/Coordinations/HealthChecks/V2/HealthV2CoordinationServiceTests.Logic.RetrieveHealthSummary.cs
@@ -830,5 +830,103 @@ namespace EventHighway.Core.Tests.Unit.Services.Coordinations.HealthChecks.V2
             this.eventArchiveV2OrchestrationServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+        [Fact]
+        public async Task ShouldReturnNAForHandlerCountWhenNoThresholdIsConfiguredAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            var configWithoutHandlerCount = new EventHighwayConfiguration();
+            configWithoutHandlerCount.Health.Thresholds.RemoveAll(
+                t => t.Metric == HealthMetric.HandlerCount);
+
+            var randomEventAddressV2s = CreateRandomEventAddressV2s();
+            var randomEventListenerV2s = CreateRandomEventListenerV2s();
+
+            var randomEventV2s = CreateRandomEventV2s(
+                immediateCount: 2,
+                scheduledCount: 1,
+                deadCount: 0);
+
+            var randomListenerEventV2s = CreateRandomListenerEventV2s(
+                successCount: 9,
+                pendingCount: 0,
+                errorCount: 0);
+
+            var emptyHandlers = CreateRandomEventHandlers(count: 0);
+            var randomEventArchiveV2s = CreateRandomEventArchiveV2s();
+
+            var randomListenerEventArchiveV2s = CreateRandomListenerEventArchiveV2s(
+                successCount: 2,
+                errorCount: 0);
+
+            this.configurationBrokerMock
+                .Setup(broker => broker.GetEventHighwayConfiguration())
+                .Returns(configWithoutHandlerCount);
+
+            this.eventV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllEventV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(randomEventV2s);
+
+            this.eventV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllEventAddressV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(randomEventAddressV2s);
+
+            this.eventListenerV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllEventListenerV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(randomEventListenerV2s);
+
+            this.eventListenerV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllListenerEventV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(randomListenerEventV2s);
+
+            this.eventListenerV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllEventHandlerV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(emptyHandlers);
+
+            this.eventArchiveV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllEventArchiveV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(randomEventArchiveV2s);
+
+            this.eventArchiveV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllListenerEventArchiveV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(randomListenerEventArchiveV2s);
+
+            // when
+            IEnumerable<HealthCheckItemV2> actualResult =
+                await this.healthV2CoordinationService
+                    .RetrieveHealthSummaryV2Async(randomCancellationToken);
+
+            // then
+            actualResult.Single(i => i.Grouping == "Event Handlers" && i.Item == "Registered Handlers")
+                .StatusCode.Should().Be((int)HealthStatusV2.NA);
+
+            this.eventV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventV2sAsync(randomCancellationToken), Times.Once);
+
+            this.eventV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventAddressV2sAsync(randomCancellationToken), Times.Once);
+
+            this.eventListenerV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventListenerV2sAsync(randomCancellationToken), Times.Once);
+
+            this.eventListenerV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllListenerEventV2sAsync(randomCancellationToken), Times.Once);
+
+            this.eventListenerV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventHandlerV2sAsync(randomCancellationToken), Times.Once);
+
+            this.eventArchiveV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventArchiveV2sAsync(randomCancellationToken), Times.Once);
+
+            this.eventArchiveV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllListenerEventArchiveV2sAsync(randomCancellationToken), Times.Once);
+
+            this.eventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.eventListenerV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.eventArchiveV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
