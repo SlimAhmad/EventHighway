a36b0ef5e2dbcef4c7d9a1bb2b1413c27f12b070
Christo du Toit <christo@dutoit.co.uk>
2026-06-15 20:42:29 +0100

    ShouldThrowValidationExceptionOnRetrieveOrRegisterIfIdIsInvalidAndLogItAsync -> PASS

--- EventHighway.Core/Services/Processings/EventAddresses/V2/EventAddressV2ProcessingService.Validations.cs
+++ EventHighway.Core/Services/Processings/EventAddresses/V2/EventAddressV2ProcessingService.Validations.cs
@@ -10,8 +10,16 @@ namespace EventHighway.Core.Services.Processings.EventAddresses.V2
 {
     internal partial class EventAddressV2ProcessingService
     {
-        private static void ValidateOnRetrieveOrRegisterEventAddressV2(EventAddressV2 eventAddressV2) =>
+        private static void ValidateOnRetrieveOrRegisterEventAddressV2(EventAddressV2 eventAddressV2)
+        {
             ValidateEventAddressV2IsNotNull(eventAddressV2);
+
+            Validate(
+                message: "Event address is invalid, fix the errors and try again.",
+
+                (Rule: IsInvalid(eventAddressV2.Id),
+                Parameter: nameof(EventAddressV2.Id)));
+        }
 
         private static void ValidateEventAddressV2IsNotNull(EventAddressV2 eventAddressV2)
         {
