b8f1fdff278a9a3b18f857ebab1e349e042b50c2
Ahmad Salim <54475330+SlimAhmad@users.noreply.github.com>
2026-06-15 14:31:34 +0100

    ShouldBulkRemoveListenerEventArchiveV2sAsync -> FAIL

--- EventHighway.Core/Services/Processings/ListenerEventArchives/V2/ListenerEventArchiveV2ProcessingService.cs
+++ EventHighway.Core/Services/Processings/ListenerEventArchives/V2/ListenerEventArchiveV2ProcessingService.cs
@@ -2,6 +2,8 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -37,5 +39,12 @@ namespace EventHighway.Core.Services.Processings.ListenerEventArchives.V2
             return await this.listenerEventArchiveV2Service
                 .AddListenerEventArchiveV2Async(listenerEventArchiveV2, cancellationToken);
         });
+
+        public async ValueTask BulkRemoveListenerEventArchiveV2sAsync(
+            IEnumerable<ListenerEventArchiveV2> listenerEventArchiveV2s, 
+            CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
