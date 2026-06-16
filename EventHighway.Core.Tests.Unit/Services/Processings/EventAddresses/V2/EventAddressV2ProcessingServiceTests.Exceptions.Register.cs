755e1defb88616c18488ead90e0820e34794e614
Christo du Toit <christo@dutoit.co.uk>
2026-06-16 06:55:21 +0100

    ShouldThrowDependencyValidationOnRegisterIfDependencyValidationErrorOccursAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Processings/EventAddresses/V2/EventAddressV2ProcessingServiceTests.Exceptions.Register.cs
@@ -0,0 +1,72 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
+using EventHighway.Core.Models.Services.Processings.EventAddresses.V2.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Processings.EventAddresses.V2
+{
+    public partial class EventAddressV2ProcessingServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationOnRegisterIfDependencyValidationErrorOccursAndLogItAsync(
+            Xeption validationException)
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventAddressV2 someEventAddressV2 =
+                CreateRandomEventAddressV2();
+
+            var expectedEventAddressV2ProcessingDependencyValidationException =
+                new EventAddressV2ProcessingDependencyValidationException(
+                    message: "Event address validation error occurred, fix the errors and try again.",
+                    innerException: validationException.InnerException as Xeption);
+
+            this.eventAddressV2ServiceMock.Setup(service =>
+                service.AddEventAddressV2Async(
+                    It.IsAny<EventAddressV2>(),
+                    randomCancellationToken))
+                        .ThrowsAsync(validationException);
+
+            // when
+            ValueTask<EventAddressV2> registerEventAddressV2Task =
+                this.eventAddressV2ProcessingService.RegisterEventAddressV2Async(
+                    someEventAddressV2,
+                    randomCancellationToken);
+
+            EventAddressV2ProcessingDependencyValidationException
+                actualEventAddressV2ProcessingDependencyValidationException =
+                    await Assert.ThrowsAsync<EventAddressV2ProcessingDependencyValidationException>(
+                        registerEventAddressV2Task.AsTask);
+
+            // then
+            actualEventAddressV2ProcessingDependencyValidationException.Should().BeEquivalentTo(
+                expectedEventAddressV2ProcessingDependencyValidationException);
+
+            this.eventAddressV2ServiceMock.Verify(service =>
+                service.AddEventAddressV2Async(
+                    It.IsAny<EventAddressV2>(),
+                    randomCancellationToken),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventAddressV2ProcessingDependencyValidationException))),
+                        Times.Once);
+
+            this.eventAddressV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
