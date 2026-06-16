77cfb068a8561254c8275a868162b4fa51632b16
Christo du Toit <christo@dutoit.co.uk>
2026-06-16 09:00:04 +0100

    ShouldThrowValidationExceptionOnRetrieveOrRegisterIfIdIsInvalidAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Processings/EventListeners/V2/EventListenerV2ProcessingServiceTests.Validations.RetrieveOrRegister.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/EventListeners/V2/EventListenerV2ProcessingServiceTests.Validations.RetrieveOrRegister.cs
@@ -59,5 +59,57 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.EventListeners.V2
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.eventListenerV2ServiceMock.VerifyNoOtherCalls();
         }
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveOrRegisterIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventListenerV2 invalidEventListenerV2 =
+                CreateRandomEventListenerV2();
+
+            invalidEventListenerV2.Id = Guid.Empty;
+
+            var invalidEventListenerV2ProcessingException =
+                new InvalidEventListenerV2ProcessingException(
+                    message: "Event listener is invalid, fix the errors and try again.");
+
+            invalidEventListenerV2ProcessingException.AddData(
+                key: nameof(EventListenerV2.Id),
+                values: "Required");
+
+            var expectedEventListenerV2ProcessingValidationException =
+                new EventListenerV2ProcessingValidationException(
+                    message: "Event listener validation error occurred, fix the errors and try again.",
+                    innerException: invalidEventListenerV2ProcessingException);
+
+            // when
+            ValueTask<EventListenerV2> retrieveOrRegisterEventListenerV2Task =
+                this.eventListenerV2ProcessingService.RetrieveOrRegisterEventListenerV2Async(
+                    invalidEventListenerV2,
+                    randomCancellationToken);
+
+            EventListenerV2ProcessingValidationException
+                actualEventListenerV2ProcessingValidationException =
+                    await Assert.ThrowsAsync<EventListenerV2ProcessingValidationException>(
+                        retrieveOrRegisterEventListenerV2Task.AsTask);
+
+            // then
+            actualEventListenerV2ProcessingValidationException.Should().BeEquivalentTo(
+                expectedEventListenerV2ProcessingValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventListenerV2ProcessingValidationException))),
+                        Times.Once);
+
+            this.eventListenerV2ServiceMock.Verify(service =>
+                service.RetrieveAllEventListenerV2sAsync(),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.eventListenerV2ServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
