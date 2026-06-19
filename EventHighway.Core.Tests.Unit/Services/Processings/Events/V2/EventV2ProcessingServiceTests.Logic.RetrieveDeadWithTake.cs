618e023c348333a1cebb4ed82ad1a5f6cd735f45
Christo du Toit <christo@dutoit.co.uk>
2026-06-19 07:58:57 +0100

    ShouldRetrieveAllDeadEventV2sWithListenersWithTakeAsync, ShouldRetrieveAllDeadEventV2sWithListenersWithoutTakeAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Logic.RetrieveDeadWithTake.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Logic.RetrieveDeadWithTake.cs
@@ -77,5 +77,67 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V2
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+        [Fact]
+        public async Task ShouldRetrieveAllDeadEventV2sWithListenersWithoutTakeAsync()
+        {
+            // given
+            int noTake = 0;
+
+            List<EventV2> randomScheduledEventV2s =
+                CreateRandomEventV2s(
+                    dates: GetRandomDateTimeOffset(),
+                    eventV2Type: EventTypeV2.Scheduled)
+                        .ToList();
+
+            List<EventV2> randomImmediateEventV2sWithRetries =
+                CreateRandomEventV2s(
+                    dates: GetRandomDateTimeOffset(),
+                    eventV2Type: EventTypeV2.Immediate)
+                        .ToList();
+
+            randomImmediateEventV2sWithRetries.ForEach(eventV2 =>
+                eventV2.RemainingRetryAttempts = GetRandomNumber());
+
+            List<EventV2> randomDeadEventV2s =
+                CreateRandomEventV2s(
+                    dates: GetRandomDateTimeOffset(),
+                    eventV2Type: EventTypeV2.Immediate)
+                        .ToList();
+
+            randomDeadEventV2s.ForEach(eventV2 =>
+            {
+                eventV2.RemainingRetryAttempts = 0;
+                eventV2.ListenerEventV2s = new List<ListenerEventV2>();
+            });
+
+            IQueryable<EventV2> retrievedEventV2s =
+                randomScheduledEventV2s
+                    .Union(randomImmediateEventV2sWithRetries)
+                    .Union(randomDeadEventV2s)
+                        .AsQueryable();
+
+            IQueryable<EventV2> expectedEventV2s =
+                randomDeadEventV2s.AsQueryable();
+
+            this.eventV2ServiceMock.Setup(service =>
+                service.RetrieveAllEventV2sWithListenerEventV2sAsync())
+                    .ReturnsAsync(retrievedEventV2s);
+
+            // when
+            IQueryable<EventV2> actualEventV2s =
+                await this.eventV2ProcessingService
+                    .RetrieveAllDeadEventV2sWithListenersAsync(noTake);
+
+            // then
+            actualEventV2s.Should().BeEquivalentTo(expectedEventV2s);
+
+            this.eventV2ServiceMock.Verify(service =>
+                service.RetrieveAllEventV2sWithListenerEventV2sAsync(),
+                    Times.Once);
+
+            this.eventV2ServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
