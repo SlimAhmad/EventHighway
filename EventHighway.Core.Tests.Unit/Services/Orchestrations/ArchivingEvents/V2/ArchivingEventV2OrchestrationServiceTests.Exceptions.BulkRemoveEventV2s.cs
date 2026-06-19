735ccab683c26936bc073cdf19318038f1e0eab0
Christo du Toit <christo@dutoit.co.uk>
2026-06-19 23:36:58 +0100

    ShouldThrowDependencyValidationExceptionOnBulkRemoveEventV2sIfValidationErrorOccursAndLogItAsync, ShouldThrowDependencyExceptionOnBulkRemoveEventV2sIfDependencyErrorOccursAndLogItAsync, ShouldThrowServiceExceptionOnBulkRemoveEventV2sIfExceptionOccursAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/ArchivingEvents/V2/ArchivingEventV2OrchestrationServiceTests.Exceptions.BulkRemoveEventV2s.cs
@@ -0,0 +1,180 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Orchestrations.ArchivingEvents.V2.Exceptions;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.ArchivingEvents.V2
+{
+    public partial class ArchivingEventV2OrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task
+            ShouldThrowDependencyValidationExceptionOnBulkRemoveEventV2sIfValidationErrorOccursAndLogItAsync(
+                Xeption validationException)
+        {
+            // given
+            IEnumerable<EventV2> someEventV2s = CreateRandomEventV2List();
+
+            var expectedArchivingEventV2OrchestrationDependencyValidationException =
+                new ArchivingEventV2OrchestrationDependencyValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: validationException.InnerException as Xeption);
+
+            this.eventV2ProcessingServiceMock.Setup(service =>
+                service.BulkRemoveEventV2sAsync(
+                    It.IsAny<IEnumerable<EventV2>>(),
+                    It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(validationException);
+
+            // when
+            ValueTask bulkRemoveEventV2sTask =
+                this.archivingEventV2OrchestrationService.BulkRemoveEventV2sAsync(
+                    someEventV2s,
+                    TestContext.Current.CancellationToken);
+
+            ArchivingEventV2OrchestrationDependencyValidationException
+                actualArchivingEventV2OrchestrationDependencyValidationException =
+                    await Assert.ThrowsAsync<ArchivingEventV2OrchestrationDependencyValidationException>(
+                        bulkRemoveEventV2sTask.AsTask);
+
+            // then
+            actualArchivingEventV2OrchestrationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedArchivingEventV2OrchestrationDependencyValidationException);
+
+            this.eventV2ProcessingServiceMock.Verify(service =>
+                service.BulkRemoveEventV2sAsync(
+                    It.IsAny<IEnumerable<EventV2>>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedArchivingEventV2OrchestrationDependencyValidationException))),
+                        Times.Once);
+
+            this.eventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.listenerEventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task
+            ShouldThrowDependencyExceptionOnBulkRemoveEventV2sIfDependencyErrorOccursAndLogItAsync(
+                Xeption dependencyException)
+        {
+            // given
+            IEnumerable<EventV2> someEventV2s = CreateRandomEventV2List();
+
+            var expectedArchivingEventV2OrchestrationDependencyException =
+                new ArchivingEventV2OrchestrationDependencyException(
+                    message: "Event dependency error occurred, contact support.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.eventV2ProcessingServiceMock.Setup(service =>
+                service.BulkRemoveEventV2sAsync(
+                    It.IsAny<IEnumerable<EventV2>>(),
+                    It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask bulkRemoveEventV2sTask =
+                this.archivingEventV2OrchestrationService.BulkRemoveEventV2sAsync(
+                    someEventV2s,
+                    TestContext.Current.CancellationToken);
+
+            ArchivingEventV2OrchestrationDependencyException
+                actualArchivingEventV2OrchestrationDependencyException =
+                    await Assert.ThrowsAsync<ArchivingEventV2OrchestrationDependencyException>(
+                        bulkRemoveEventV2sTask.AsTask);
+
+            // then
+            actualArchivingEventV2OrchestrationDependencyException.Should()
+                .BeEquivalentTo(expectedArchivingEventV2OrchestrationDependencyException);
+
+            this.eventV2ProcessingServiceMock.Verify(service =>
+                service.BulkRemoveEventV2sAsync(
+                    It.IsAny<IEnumerable<EventV2>>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedArchivingEventV2OrchestrationDependencyException))),
+                        Times.Once);
+
+            this.eventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.listenerEventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnBulkRemoveEventV2sIfExceptionOccursAndLogItAsync()
+        {
+            // given
+            IEnumerable<EventV2> someEventV2s = CreateRandomEventV2List();
+            var exception = new Exception();
+            exception.Data.Add("ErrorCode", new List<string> { "ServiceError" });
+
+            var failedArchivingEventV2OrchestrationServiceException =
+                new FailedArchivingEventV2OrchestrationServiceException(
+                    message: "Failed event service error occurred, contact support.",
+                    innerException: exception,
+                    data: exception.Data);
+
+            var expectedArchivingEventV2OrchestrationServiceException =
+                new ArchivingEventV2OrchestrationServiceException(
+                    message: "Event service error occurred, contact support.",
+                    innerException: failedArchivingEventV2OrchestrationServiceException);
+
+            this.eventV2ProcessingServiceMock.Setup(service =>
+                service.BulkRemoveEventV2sAsync(
+                    It.IsAny<IEnumerable<EventV2>>(),
+                    It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(exception);
+
+            // when
+            ValueTask bulkRemoveEventV2sTask =
+                this.archivingEventV2OrchestrationService.BulkRemoveEventV2sAsync(
+                    someEventV2s,
+                    TestContext.Current.CancellationToken);
+
+            ArchivingEventV2OrchestrationServiceException
+                actualArchivingEventV2OrchestrationServiceException =
+                    await Assert.ThrowsAsync<ArchivingEventV2OrchestrationServiceException>(
+                        bulkRemoveEventV2sTask.AsTask);
+
+            // then
+            actualArchivingEventV2OrchestrationServiceException.Should()
+                .BeEquivalentTo(expectedArchivingEventV2OrchestrationServiceException);
+
+            this.eventV2ProcessingServiceMock.Verify(service =>
+                service.BulkRemoveEventV2sAsync(
+                    It.IsAny<IEnumerable<EventV2>>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedArchivingEventV2OrchestrationServiceException))),
+                        Times.Once);
+
+            this.eventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.listenerEventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
