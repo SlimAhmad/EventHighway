6efd3163d632e6978fff49515db051861235e90f
Christo du Toit <christo@dutoit.co.uk>
2026-06-19 13:26:34 +0100

    ShouldRetrieveBatchOfListenerEventV2sByEventIdsWithoutTakeAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Processings/ListenerEvents/V2/ListenerEventV2ProcessingServiceTests.Logic.RetrieveBatchByEventIds.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/ListenerEvents/V2/ListenerEventV2ProcessingServiceTests.Logic.RetrieveBatchByEventIds.cs
@@ -15,6 +15,43 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.ListenerEvents.V2
     public partial class ListenerEventV2ProcessingServiceTests
     {
         [Fact]
+        public async Task ShouldRetrieveBatchOfListenerEventV2sByEventIdsWithoutTakeAsync()
+        {
+            // given
+            List<Guid> someEventIds =
+                Enumerable.Range(0, GetRandomNumber())
+                    .Select(_ => Guid.NewGuid())
+                        .ToList();
+
+            int inputTake = 0;
+
+            IQueryable<ListenerEventV2> retrievedListenerEventV2s =
+                CreateRandomListenerEventV2s();
+
+            IEnumerable<ListenerEventV2> expectedListenerEventV2s =
+                retrievedListenerEventV2s.AsEnumerable();
+
+            this.listenerEventV2ServiceMock.Setup(service =>
+                service.RetrieveListenerEventV2sByEventIdsAsync(someEventIds))
+                    .ReturnsAsync(retrievedListenerEventV2s);
+
+            // when
+            IEnumerable<ListenerEventV2> actualListenerEventV2s =
+                await this.listenerEventV2ProcessingService
+                    .RetrieveBatchOfListenerEventV2sByEventIdsAsync(someEventIds, inputTake);
+
+            // then
+            actualListenerEventV2s.Should().BeEquivalentTo(expectedListenerEventV2s);
+
+            this.listenerEventV2ServiceMock.Verify(service =>
+                service.RetrieveListenerEventV2sByEventIdsAsync(someEventIds),
+                    Times.Once);
+
+            this.listenerEventV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
         public async Task ShouldRetrieveBatchOfListenerEventV2sByEventIdsWithTakeAsync()
         {
             // given
