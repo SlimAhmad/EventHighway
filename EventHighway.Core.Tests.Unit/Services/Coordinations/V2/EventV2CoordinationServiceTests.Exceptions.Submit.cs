d4752fc75a8d69ab4650bbc299f9dd120af5fd60
Christo du Toit <christo@dutoit.co.uk>
2026-06-20 20:53:44 +0100

    ShouldThrowDependencyValidationOnSubmitIfDependencyValidationErrorOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Coordinations/V2/EventV2CoordinationServiceTests.Exceptions.Submit.cs
+++ EventHighway.Core.Tests.Unit/Services/Coordinations/V2/EventV2CoordinationServiceTests.Exceptions.Submit.cs
@@ -23,6 +23,9 @@ namespace EventHighway.Core.Tests.Unit.Services.Coordinations.V2
             Xeption validationException)
         {
             // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
             EventV2 someEventV2 = CreateRandomEventV2();
 
             var expectedEventV2CoordinationDependencyValidationException =
@@ -38,7 +41,7 @@ namespace EventHighway.Core.Tests.Unit.Services.Coordinations.V2
             ValueTask<EventV2> submitEventV2Task =
                 this.eventV2CoordinationService.SubmitEventV2Async(
                     someEventV2,
-                    TestContext.Current.CancellationToken);
+                    randomCancellationToken);
 
             EventV2CoordinationDependencyValidationException
                 actualEventV2CoordinationDependencyValidationException =
