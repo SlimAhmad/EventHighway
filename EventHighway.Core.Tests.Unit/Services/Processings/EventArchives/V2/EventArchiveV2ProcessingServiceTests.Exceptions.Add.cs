7559ee7b5ab5be343ca93a5d115f4d1332bcc4a2
Christo du Toit <christo@dutoit.co.uk>
2026-06-20 20:47:55 +0100

    ShouldThrowDependencyExceptionOnAddIfDependencyExceptionOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Processings/EventArchives/V2/EventArchiveV2ProcessingServiceTests.Exceptions.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/EventArchives/V2/EventArchiveV2ProcessingServiceTests.Exceptions.Add.cs
@@ -74,6 +74,9 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.EventArchives.V2
             Xeption dependencyException)
         {
             // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
             EventArchiveV2 someEventArchiveV2 = CreateRandomEventArchiveV2();
 
             var expectedEventArchiveV2ProcessingDependencyException =
@@ -91,7 +94,7 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.EventArchives.V2
             ValueTask<EventArchiveV2> addEventArchiveV2Task =
                 this.eventArchiveV2ProcessingService.AddEventArchiveV2Async(
                     someEventArchiveV2,
-                    TestContext.Current.CancellationToken);
+                    randomCancellationToken);
 
             EventArchiveV2ProcessingDependencyException
                 actualEventArchiveV2ProcessingDependencyException =
