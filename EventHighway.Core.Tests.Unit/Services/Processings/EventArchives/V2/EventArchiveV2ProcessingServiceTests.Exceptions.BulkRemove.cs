c2708841122952c2771a9478d5618c391c1263d7
Christo du Toit <christo@dutoit.co.uk>
2026-06-20 06:42:39 +0100

    ShouldThrowServiceExceptionOnBulkRemoveIfExceptionOccursAndLogItAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Processings/EventArchives/V2/EventArchiveV2ProcessingServiceTests.Exceptions.BulkRemove.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/EventArchives/V2/EventArchiveV2ProcessingServiceTests.Exceptions.BulkRemove.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
