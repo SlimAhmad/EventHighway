3e4db7e8f0420f736a34ac1459ba5168e09c5deb
Christo du Toit <christo@dutoit.co.uk>
2026-06-20 20:48:27 +0100

    ShouldBulkAddEventArchiveV2sAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V2/EventArchiveV2OrchestrationServiceTests.Logic.BulkAdd.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V2/EventArchiveV2OrchestrationServiceTests.Logic.BulkAdd.cs
@@ -18,6 +18,9 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V2
         public async Task ShouldBulkAddEventArchiveV2sAsync()
         {
             // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
             IQueryable<EventArchiveV2> randomEventArchiveV2s = CreateRandomEventArchiveV2s();
             IEnumerable<EventArchiveV2> inputEventArchiveV2s = randomEventArchiveV2s;
             IEnumerable<EventArchiveV2> addedEventArchiveV2s = inputEventArchiveV2s;
@@ -25,14 +28,14 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V2
             this.eventArchiveV2ProcessingServiceMock.Setup(service =>
                 service.BulkAddEventArchiveV2sAsync(
                     inputEventArchiveV2s,
-                    It.IsAny<CancellationToken>()))
+                    randomCancellationToken))
                         .ReturnsAsync(addedEventArchiveV2s);
 
             // when
             IEnumerable<EventArchiveV2> actualAddedEventArchiveV2s =
                 await this.eventArchiveV2OrchestrationService.BulkAddEventArchiveV2sAsync(
                     inputEventArchiveV2s,
-                    TestContext.Current.CancellationToken);
+                    randomCancellationToken);
 
             // then
             actualAddedEventArchiveV2s.Should().BeEquivalentTo(addedEventArchiveV2s);
@@ -40,7 +43,7 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V2
             this.eventArchiveV2ProcessingServiceMock.Verify(service =>
                 service.BulkAddEventArchiveV2sAsync(
                     inputEventArchiveV2s,
-                    It.IsAny<CancellationToken>()),
+                    randomCancellationToken),
                         Times.Once);
 
             this.listenerEventArchiveV2ProcessingServiceMock.VerifyNoOtherCalls();
