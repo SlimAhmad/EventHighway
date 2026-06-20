9dab75c8c140b4a88a5612c40b37338ae4c0a59d
Christo du Toit <christo@dutoit.co.uk>
2026-06-20 20:56:38 +0100

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventArchives/V2/EventArchiveV2ServiceTests.Validations.RetrieveById.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventArchives/V2/EventArchiveV2ServiceTests.Validations.RetrieveById.cs
@@ -18,7 +18,7 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventArchives.V2
         public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
         {
             // given
-            CancellationToken cancellationToken =
+            CancellationToken randomCancellationToken =
                 TestContext.Current.CancellationToken;
 
             Guid invalidEventArchiveV2Id = Guid.Empty;
@@ -40,7 +40,7 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventArchives.V2
             ValueTask<EventArchiveV2> retrieveEventArchiveV2ByIdTask =
                 this.eventArchiveV2Service.RetrieveEventArchiveV2ByIdAsync(
                     invalidEventArchiveV2Id,
-                    cancellationToken);
+                    randomCancellationToken);
 
             EventArchiveV2ValidationException actualEventArchiveV2ValidationException =
                 await Assert.ThrowsAsync<EventArchiveV2ValidationException>(
