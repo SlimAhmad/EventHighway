d08b63a92e80884951f1f0bfb9b274745373dc85
Christo du Toit <christo@dutoit.co.uk>
2026-06-20 20:48:04 +0100

    ShouldThrowDependencyExceptionOnRunIfServiceExceptionOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventCalls/V2/EventCallV2ServiceTests.Exceptions.RunEventCallV2.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventCalls/V2/EventCallV2ServiceTests.Exceptions.RunEventCallV2.cs
@@ -178,6 +178,9 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventCalls.V2
             Exception serviceException)
         {
             // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
             EventCallV2 someEventCallV2 = CreateRandomEventCallV2();
             serviceException.Data.Add("ErrorCode", new List<string> { "ServiceError" });
 
@@ -210,7 +213,7 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventCalls.V2
 
             // when
             ValueTask<EventCallV2> runEventCallV2Task =
-                this.eventCallV2Service.RunEventCallV2Async(someEventCallV2, TestContext.Current.CancellationToken);
+                this.eventCallV2Service.RunEventCallV2Async(someEventCallV2, randomCancellationToken);
 
             EventCallV2DependencyException actualEventCallV2DependencyException =
                 await Assert.ThrowsAsync<EventCallV2DependencyException>(
