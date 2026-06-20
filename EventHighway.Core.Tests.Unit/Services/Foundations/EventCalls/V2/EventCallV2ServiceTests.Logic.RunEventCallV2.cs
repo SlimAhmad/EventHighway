7ffe506894da4ebfa010c0332cc014756995054c
Christo du Toit <christo@dutoit.co.uk>
2026-06-20 20:49:22 +0100

    ShouldSkipEventCallV2WhenFilterCriteriaDoesNotMatchAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventCalls/V2/EventCallV2ServiceTests.Logic.RunEventCallV2.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventCalls/V2/EventCallV2ServiceTests.Logic.RunEventCallV2.cs
@@ -100,6 +100,9 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventCalls.V2
         public async Task ShouldSkipEventCallV2WhenFilterCriteriaDoesNotMatchAsync()
         {
             // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
             string randomHandlerName = GetRandomString();
             string promotedPropertyName = "OrderType";
             string promotedPropertyValue = "Standard";
@@ -140,13 +143,13 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventCalls.V2
                 .Setup(handler =>
                     handler.HandleAsync(
                         It.IsAny<string>(),
-                        It.IsAny<CancellationToken>()))
+                        randomCancellationToken))
                 .ReturnsAsync(CreateRandomEventHandlerResult());
 
             // when
             EventCallV2 actualEventCallV2 =
                 await this.eventCallV2Service
-                    .RunEventCallV2Async(inputEventCallV2, TestContext.Current.CancellationToken);
+                    .RunEventCallV2Async(inputEventCallV2, randomCancellationToken);
 
             // then
             actualEventCallV2.Should().BeEquivalentTo(expectedEventCallV2);
@@ -165,7 +168,7 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventCalls.V2
             this.eventHandlerMock.Verify(handler =>
                 handler.HandleAsync(
                     It.IsAny<string>(),
-                    It.IsAny<CancellationToken>()),
+                    randomCancellationToken),
                 Times.Never);
 
             this.eventHandlerMock.VerifyNoOtherCalls();
