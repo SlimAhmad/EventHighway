a9646311e96b862a41079204cf940407cf2c26b3
Christo du Toit <christo@dutoit.co.uk>
2026-06-20 06:44:41 +0100

    ShouldRetrieveBatchOfEventArchiveV2sOlderThanWithTakeAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Processings/EventArchives/V2/EventArchiveV2ProcessingServiceTests.Logic.RetrieveBatchOlderThan.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/EventArchives/V2/EventArchiveV2ProcessingServiceTests.Logic.RetrieveBatchOlderThan.cs
@@ -51,5 +51,44 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.EventArchives.V2
             this.eventArchiveV2ServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+        [Fact]
+        public async Task ShouldRetrieveBatchOfEventArchiveV2sOlderThanWithTakeAsync()
+        {
+            // given
+            DateTimeOffset randomOlderThan = GetRandomDateTimeOffset();
+            DateTimeOffset inputOlderThan = randomOlderThan;
+            int randomTake = GetRandomNumber();
+            int inputTake = randomTake;
+
+            IQueryable<EventArchiveV2> allEventArchiveV2s =
+                CreateRandomEventArchiveV2s();
+
+            IEnumerable<EventArchiveV2> expectedEventArchiveV2s =
+                allEventArchiveV2s
+                    .Where(archive => archive.ArchivedDate < inputOlderThan)
+                    .Take(inputTake)
+                    .AsEnumerable();
+
+            this.eventArchiveV2ServiceMock.Setup(service =>
+                service.RetrieveAllEventArchiveV2sAsync())
+                    .ReturnsAsync(allEventArchiveV2s);
+
+            // when
+            IEnumerable<EventArchiveV2> actualEventArchiveV2s =
+                await this.eventArchiveV2ProcessingService
+                    .RetrieveBatchOfEventArchiveV2sOlderThanAsync(
+                        inputOlderThan,
+                        inputTake);
+
+            // then
+            actualEventArchiveV2s.Should().BeEquivalentTo(expectedEventArchiveV2s);
+
+            this.eventArchiveV2ServiceMock.Verify(service =>
+                service.RetrieveAllEventArchiveV2sAsync(),
+                    Times.Once);
+
+            this.eventArchiveV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
