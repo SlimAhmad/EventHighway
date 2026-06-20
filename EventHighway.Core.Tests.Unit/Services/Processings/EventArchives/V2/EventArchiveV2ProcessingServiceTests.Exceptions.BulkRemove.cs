202a20bb588ad34c920bb16c145e516f47a38b10
Christo du Toit <christo@dutoit.co.uk>
2026-06-20 06:37:57 +0100

    ShouldThrowDependencyExceptionOnBulkRemoveIfDependencyErrorOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Processings/EventArchives/V2/EventArchiveV2ProcessingServiceTests.Exceptions.BulkRemove.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/EventArchives/V2/EventArchiveV2ProcessingServiceTests.Exceptions.BulkRemove.cs
@@ -65,5 +65,55 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.EventArchives.V2
             this.eventArchiveV2ServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnBulkRemoveIfDependencyErrorOccursAndLogItAsync(
+            Xeption eventArchiveV2DependencyException)
+        {
+            // given
+            IEnumerable<EventArchiveV2> someEventArchiveV2s =
+                CreateRandomEventArchiveV2s().ToList();
+
+            var expectedEventArchiveV2ProcessingDependencyException =
+                new EventArchiveV2ProcessingDependencyException(
+                    message: "Event archive dependency error occurred, contact support.",
+                    innerException: eventArchiveV2DependencyException.InnerException as Xeption);
+
+            this.eventArchiveV2ServiceMock.Setup(service =>
+                service.BulkRemoveEventArchiveV2sAsync(
+                    someEventArchiveV2s,
+                    TestContext.Current.CancellationToken))
+                        .ThrowsAsync(eventArchiveV2DependencyException);
+
+            // when
+            ValueTask bulkRemoveEventArchiveV2sTask =
+                this.eventArchiveV2ProcessingService
+                    .BulkRemoveEventArchiveV2sAsync(
+                        someEventArchiveV2s,
+                        TestContext.Current.CancellationToken);
+
+            EventArchiveV2ProcessingDependencyException
+                actualEventArchiveV2ProcessingDependencyException =
+                    await Assert.ThrowsAsync<EventArchiveV2ProcessingDependencyException>(
+                        bulkRemoveEventArchiveV2sTask.AsTask);
+
+            // then
+            actualEventArchiveV2ProcessingDependencyException.Should()
+                .BeEquivalentTo(expectedEventArchiveV2ProcessingDependencyException);
+
+            this.eventArchiveV2ServiceMock.Verify(service =>
+                service.BulkRemoveEventArchiveV2sAsync(
+                    someEventArchiveV2s,
+                    TestContext.Current.CancellationToken),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventArchiveV2ProcessingDependencyException))),
+                        Times.Once);
+
+            this.eventArchiveV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
