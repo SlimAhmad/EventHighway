a3f178e4f988013fe48bb90fca57b2d83fef1722
Christo du Toit <christo@dutoit.co.uk>
2026-06-20 21:16:12 +0100

    ShouldThrowDependencyExceptionOnRetrieveByIdIfTimeoutOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Processings/EventAddresses/V2/EventAddressV2ProcessingServiceTests.Exceptions.RetrieveById.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/EventAddresses/V2/EventAddressV2ProcessingServiceTests.Exceptions.RetrieveById.cs
@@ -177,5 +177,97 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.EventAddresses.V2
             this.eventAddressV2ServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveByIdIfTimeoutOccursAndLogItAsync()
+        {
+            // given
+            Guid someEventAddressV2Id = GetRandomId();
+            var operationCanceledException = new OperationCanceledException();
+
+            var timeoutException =
+                new TimeoutException("The dependency operation timed out.");
+
+            var timeoutEventAddressV2ProcessingException =
+                new TimeoutEventAddressV2ProcessingException(
+                    message: "Failed event address processing timeout error occurred, contact support.",
+                    innerException: timeoutException,
+                    data: timeoutException.Data);
+
+            var expectedEventAddressV2ProcessingDependencyException =
+                new EventAddressV2ProcessingDependencyException(
+                    message: "Event address dependency error occurred, contact support.",
+                    innerException: timeoutEventAddressV2ProcessingException);
+
+            this.eventAddressV2ServiceMock.Setup(service =>
+                service.RetrieveEventAddressV2ByIdAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(operationCanceledException);
+
+            // when
+            ValueTask<EventAddressV2> retrieveEventAddressV2ByIdTask =
+                this.eventAddressV2ProcessingService.RetrieveEventAddressV2ByIdAsync(
+                    someEventAddressV2Id);
+
+            EventAddressV2ProcessingDependencyException
+                actualEventAddressV2ProcessingDependencyException =
+                    await Assert.ThrowsAsync<EventAddressV2ProcessingDependencyException>(
+                        retrieveEventAddressV2ByIdTask.AsTask);
+
+            // then
+            actualEventAddressV2ProcessingDependencyException.Should().BeEquivalentTo(
+                expectedEventAddressV2ProcessingDependencyException);
+
+            this.eventAddressV2ServiceMock.Verify(service =>
+                service.RetrieveEventAddressV2ByIdAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventAddressV2ProcessingDependencyException))),
+                        Times.Once);
+
+            this.eventAddressV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowOperationCanceledExceptionRawWhenCancellationIsRequestedOnRetrieveByIdAsync()
+        {
+            // given
+            Guid someEventAddressV2Id = GetRandomId();
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.Cancel();
+            CancellationToken cancelledToken = cancellationTokenSource.Token;
+
+            // when
+            ValueTask<EventAddressV2> retrieveEventAddressV2ByIdTask =
+                this.eventAddressV2ProcessingService.RetrieveEventAddressV2ByIdAsync(
+                    someEventAddressV2Id,
+                    cancelledToken);
+
+            // then
+            OperationCanceledException actualException =
+                await Assert.ThrowsAsync<OperationCanceledException>(
+                    retrieveEventAddressV2ByIdTask.AsTask);
+
+            actualException.Should().NotBeOfType<EventAddressV2ProcessingDependencyException>();
+            actualException.Should().NotBeOfType<EventAddressV2ProcessingServiceException>();
+            actualException.CancellationToken.IsCancellationRequested.Should().BeTrue();
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.IsAny<Xeption>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.IsAny<Xeption>()),
+                    Times.Never);
+
+            this.eventAddressV2ServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
