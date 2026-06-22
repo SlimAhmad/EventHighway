15253cf0e2178ad527edd252203626808f62ae21
Christo du Toit <christo@dutoit.co.uk>
2026-06-22 14:58:05 +0100

    ShouldThrowValidationExceptionOnIsLoopDetectedIfEventV2IsNullAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/Events/V2/EventV2OrchestrationServiceTests.Validations.IsLoopDetected.cs
@@ -0,0 +1,66 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using EventHighway.Core.Models.Services.Orchestrations.Events.V2.Exceptions;
+using FluentAssertions;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.Events.V2
+{
+    public partial class EventV2OrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnIsLoopDetectedIfEventV2IsNullAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventV2 nullEventV2 = null;
+
+            var nullEventV2OrchestrationException =
+                new NullEventV2OrchestrationException(message: "Event is null.");
+
+            var expectedEventV2OrchestrationValidationException =
+                new EventV2OrchestrationValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: nullEventV2OrchestrationException);
+
+            // when
+            ValueTask<bool> isLoopDetectedTask =
+                this.eventV2OrchestrationService.IsLoopDetectedAsync(
+                    nullEventV2,
+                    randomCancellationToken);
+
+            EventV2OrchestrationValidationException
+                actualEventV2OrchestrationValidationException =
+                    await Assert.ThrowsAsync<EventV2OrchestrationValidationException>(
+                        isLoopDetectedTask.AsTask);
+
+            // then
+            actualEventV2OrchestrationValidationException.Should().BeEquivalentTo(
+                expectedEventV2OrchestrationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2OrchestrationValidationException))),
+                        Times.Once);
+
+            this.eventV2ProcessingServiceMock.Verify(service =>
+                service.IsLoopDetectedAsync(
+                    It.IsAny<EventV2>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.eventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.eventAddressV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.eventCallV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.hashBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
