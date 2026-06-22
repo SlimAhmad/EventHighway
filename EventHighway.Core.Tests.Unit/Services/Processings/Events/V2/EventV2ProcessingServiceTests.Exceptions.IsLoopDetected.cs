26b37bad4cc8c7e3ce89c329f5a5fa214b1ef8f1
Christo du Toit <christo@dutoit.co.uk>
2026-06-22 10:31:33 +0100

    ShouldThrowOperationCanceledExceptionRawWhenCancellationIsRequestedOnIsLoopDetectedAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Exceptions.IsLoopDetected.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Exceptions.IsLoopDetected.cs
@@ -213,5 +213,44 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V2
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowOperationCanceledExceptionRawWhenCancellationIsRequestedOnIsLoopDetectedAsync()
+        {
+            // given
+            EventV2 someEventV2 = CreateRandomEventV2();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.Cancel();
+            CancellationToken cancelledToken = cancellationTokenSource.Token;
+
+            // when
+            ValueTask<bool> isLoopDetectedTask =
+                this.eventV2ProcessingService.IsLoopDetectedAsync(
+                    someEventV2,
+                    cancelledToken);
+
+            // then
+            OperationCanceledException actualException =
+                await Assert.ThrowsAsync<OperationCanceledException>(
+                    isLoopDetectedTask.AsTask);
+
+            actualException.Should().NotBeOfType<EventV2ProcessingDependencyException>();
+            actualException.Should().NotBeOfType<EventV2ProcessingServiceException>();
+            actualException.CancellationToken.IsCancellationRequested.Should().BeTrue();
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.IsAny<Xeption>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.IsAny<Xeption>()),
+                    Times.Never);
+
+            this.eventV2ServiceMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
