81183a19c92f1a7ef39ed0d023b488c95c7a4c6e
Christo du Toit <christo@dutoit.co.uk>
2026-06-22 10:29:37 +0100

    ShouldThrowDependencyExceptionOnIsLoopDetectedIfTimeoutOccursAndLogItAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Exceptions.IsLoopDetected.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Exceptions.IsLoopDetected.cs
@@ -80,6 +80,7 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V2
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
         [Theory]
         [MemberData(nameof(DependencyExceptions))]
         public async Task
@@ -123,6 +124,74 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V2
             // then
             actualEventV2ProcessingDependencyException.Should()
                 .BeEquivalentTo(expectedEventV2ProcessingDependencyException);
+
+            this.configurationBrokerMock.Verify(broker =>
+                broker.GetLoopDetectionConfiguration(),
+                    Times.Once);
+
+            this.eventV2ServiceMock.Verify(service =>
+                service.RetrieveEventV2CountBySignatureAsync(
+                    It.IsAny<EventV2>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2ProcessingDependencyException))),
+                        Times.Once);
+
+            this.eventV2ServiceMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnIsLoopDetectedIfTimeoutOccursAndLogItAsync()
+        {
+            // given
+            EventV2 someEventV2 = CreateRandomEventV2();
+            var operationCanceledException = new OperationCanceledException();
+
+            var loopDetectionConfig = new LoopDetection { Enabled = true };
+
+            this.configurationBrokerMock
+                .Setup(broker => broker.GetLoopDetectionConfiguration())
+                    .Returns(loopDetectionConfig);
+
+            var timeoutException =
+                new TimeoutException("The dependency operation timed out.");
+
+            var timeoutEventV2ProcessingException =
+                new TimeoutEventV2ProcessingException(
+                    message: "Failed event processing timeout error occurred, contact support.",
+                    innerException: timeoutException,
+                    data: timeoutException.Data);
+
+            var expectedEventV2ProcessingDependencyException =
+                new EventV2ProcessingDependencyException(
+                    message: "Event dependency error occurred, contact support.",
+                    innerException: timeoutEventV2ProcessingException);
+
+            this.eventV2ServiceMock.Setup(service =>
+                service.RetrieveEventV2CountBySignatureAsync(
+                    It.IsAny<EventV2>(),
+                    It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(operationCanceledException);
+
+            // when
+            ValueTask<bool> isLoopDetectedTask =
+                this.eventV2ProcessingService.IsLoopDetectedAsync(
+                    someEventV2,
+                    TestContext.Current.CancellationToken);
+
+            EventV2ProcessingDependencyException actualEventV2ProcessingDependencyException =
+                await Assert.ThrowsAsync<EventV2ProcessingDependencyException>(
+                    isLoopDetectedTask.AsTask);
+
+            // then
+            actualEventV2ProcessingDependencyException.Should().BeEquivalentTo(
+                expectedEventV2ProcessingDependencyException);
 
             this.configurationBrokerMock.Verify(broker =>
                 broker.GetLoopDetectionConfiguration(),
