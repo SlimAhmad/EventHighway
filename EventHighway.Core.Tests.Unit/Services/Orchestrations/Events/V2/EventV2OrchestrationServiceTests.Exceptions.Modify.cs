b182ede04721e85dd91f6d7a2ee8815467c9f506
Christo du Toit <christo@dutoit.co.uk>
2026-06-22 15:56:24 +0100

    ShouldThrowDependencyValidationExceptionOnModifyIfValidationErrorOccursAndLogItAsync -> PASS

---
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/Events/V2/EventV2OrchestrationServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,74 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using EventHighway.Core.Models.Services.Orchestrations.Events.V2.Exceptions;
+using FluentAssertions;
+using Moq;
+using Xeptions;
+
+namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.Events.V2
+{
+    public partial class EventV2OrchestrationServiceTests
+    {
+        [Theory]
+        [MemberData(nameof(DependencyValidationExceptions))]
+        public async Task ShouldThrowDependencyValidationExceptionOnModifyIfValidationErrorOccursAndLogItAsync(
+            Xeption validationException)
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventV2 someEventV2 = CreateRandomEventV2();
+
+            var expectedEventV2OrchestrationDependencyValidationException =
+                new EventV2OrchestrationDependencyValidationException(
+                    message: "Event validation error occurred, fix the errors and try again.",
+                    innerException: validationException.InnerException as Xeption);
+
+            this.eventV2ProcessingServiceMock.Setup(service =>
+                service.ModifyEventV2Async(
+                    It.IsAny<EventV2>(),
+                    It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(validationException);
+
+            // when
+            ValueTask<EventV2> modifyEventV2Task =
+                this.eventV2OrchestrationService.ModifyEventV2Async(
+                    someEventV2,
+                    randomCancellationToken);
+
+            EventV2OrchestrationDependencyValidationException
+                actualEventV2OrchestrationDependencyValidationException =
+                    await Assert.ThrowsAsync<EventV2OrchestrationDependencyValidationException>(
+                        modifyEventV2Task.AsTask);
+
+            // then
+            actualEventV2OrchestrationDependencyValidationException.Should()
+                .BeEquivalentTo(expectedEventV2OrchestrationDependencyValidationException);
+
+            this.eventV2ProcessingServiceMock.Verify(broker =>
+                broker.ModifyEventV2Async(
+                    It.IsAny<EventV2>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventV2OrchestrationDependencyValidationException))),
+                        Times.Once);
+
+            this.eventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.eventAddressV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.eventCallV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.hashBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
