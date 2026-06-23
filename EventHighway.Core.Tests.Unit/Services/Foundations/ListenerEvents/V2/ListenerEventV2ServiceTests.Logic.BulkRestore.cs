9d825ef3694cb06322f04c91d25043f40d0f6ff3
Christo du Toit <christo@dutoit.co.uk>
2026-06-23 20:56:28 +0100

    ShouldBulkRestoreValidListenerEventV2sAndLogFutureDatedOnesAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Logic.BulkRestore.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Logic.BulkRestore.cs
@@ -212,5 +212,84 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.ListenerEvents.V2
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldBulkRestoreValidListenerEventV2sAndLogFutureDatedOnesAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            DateTimeOffset now = GetRandomDateTimeOffset();
+
+            List<ListenerEventV2> validListenerEventV2s =
+                CreateRandomRestoreListenerEventV2s();
+
+            foreach (ListenerEventV2 validListenerEventV2 in validListenerEventV2s)
+            {
+                validListenerEventV2.CreatedDate = now.AddMinutes(-GetRandomNumber() - 2);
+                validListenerEventV2.UpdatedDate = now.AddMinutes(-1);
+            }
+
+            ListenerEventV2 futureDatedListenerEventV2 =
+                CreateRandomRestoreListenerEventV2s().First();
+
+            futureDatedListenerEventV2.CreatedDate = now.AddMinutes(-1);
+            futureDatedListenerEventV2.UpdatedDate = now.AddMinutes(GetRandomNumber());
+
+            List<ListenerEventV2> inputListenerEventV2s =
+                validListenerEventV2s.Append(futureDatedListenerEventV2).ToList();
+
+            List<ListenerEventV2> expectedListenerEventV2s =
+                validListenerEventV2s.Select(item => item.DeepClone()).ToList();
+
+            var invalidListenerEventV2Exception =
+                new InvalidListenerEventV2Exception(
+                    message: "Listener event is invalid, fix the errors and try again.");
+
+            invalidListenerEventV2Exception.AddData(
+                key: nameof(ListenerEventV2.UpdatedDate),
+                values: "Date is in the future");
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(now);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.BulkInsertListenerEventV2sAsync(
+                    It.Is<List<ListenerEventV2>>(actual =>
+                        SameListenerEventV2sAs(expectedListenerEventV2s, actual)),
+                            randomCancellationToken))
+                                .Returns(ValueTask.CompletedTask);
+
+            // when
+            IEnumerable<ListenerEventV2> actualListenerEventV2s =
+                await this.listenerEventV2Service.BulkRestoreListenerEventV2sAsync(
+                    inputListenerEventV2s,
+                        randomCancellationToken);
+
+            // then
+            actualListenerEventV2s.Should().BeEquivalentTo(expectedListenerEventV2s);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    invalidListenerEventV2Exception))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.BulkInsertListenerEventV2sAsync(
+                    It.Is<List<ListenerEventV2>>(actual =>
+                        SameListenerEventV2sAs(expectedListenerEventV2s, actual)),
+                            randomCancellationToken),
+                                Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
