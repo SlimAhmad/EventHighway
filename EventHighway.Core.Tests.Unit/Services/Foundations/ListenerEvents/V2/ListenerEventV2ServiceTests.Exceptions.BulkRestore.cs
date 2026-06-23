945c76ccf5383bf7caa0af089828838bbbb56558
Christo du Toit <christo@dutoit.co.uk>
2026-06-23 21:00:50 +0100

    ShouldThrowCriticalDependencyExceptionOnBulkRestoreIfSqlExceptionOccursAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Exceptions.BulkRestore.cs
@@ -0,0 +1,85 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.ListenerEvents.V2;
+using EventHighway.Core.Models.Services.Foundations.ListenerEvents.V2.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.ListenerEvents.V2
+{
+    public partial class ListenerEventV2ServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnBulkRestoreIfSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            List<ListenerEventV2> someListenerEventV2s = CreateRandomRestoreListenerEventV2s();
+            SqlException sqlException = GetSqlException();
+            sqlException.Data.Add("ErrorCode", new List<string> { "SqlError" });
+
+            var failedStorageListenerEventV2Exception =
+                new FailedStorageListenerEventV2Exception(
+                    message: "Failed listener event storage error occurred, contact support.",
+                    innerException: sqlException,
+                    data: sqlException.Data);
+
+            var expectedListenerEventV2DependencyException =
+                new ListenerEventV2DependencyException(
+                    message: "Listener event dependency error occurred, contact support.",
+                    innerException: failedStorageListenerEventV2Exception);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(DateTimeOffset.MaxValue);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.BulkInsertListenerEventV2sAsync(
+                    It.IsAny<List<ListenerEventV2>>(),
+                    It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IEnumerable<ListenerEventV2>> bulkRestoreListenerEventV2sTask =
+                this.listenerEventV2Service.BulkRestoreListenerEventV2sAsync(
+                    someListenerEventV2s,
+                        randomCancellationToken);
+
+            ListenerEventV2DependencyException actualListenerEventV2DependencyException =
+                await Assert.ThrowsAsync<ListenerEventV2DependencyException>(
+                    bulkRestoreListenerEventV2sTask.AsTask);
+
+            // then
+            actualListenerEventV2DependencyException.Should()
+                .BeEquivalentTo(expectedListenerEventV2DependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.BulkInsertListenerEventV2sAsync(
+                    It.IsAny<List<ListenerEventV2>>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedListenerEventV2DependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
