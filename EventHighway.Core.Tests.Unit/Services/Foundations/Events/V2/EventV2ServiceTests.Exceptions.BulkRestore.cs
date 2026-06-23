95898f314c69c88d480ce90435c410d12bd4f495
Christo du Toit <christo@dutoit.co.uk>
2026-06-23 20:34:21 +0100

    ShouldThrowCriticalDependencyExceptionOnBulkRestoreIfSqlExceptionOccursAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Foundations/Events/V2/EventV2ServiceTests.Exceptions.BulkRestore.cs
@@ -0,0 +1,85 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using EventHighway.Core.Models.Services.Foundations.Events.V2.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Foundations.Events.V2
+{
+    public partial class EventV2ServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnBulkRestoreIfSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            List<EventV2> someEventV2s = CreateRandomRestoreEventV2s();
+            SqlException sqlException = GetSqlException();
+            sqlException.Data.Add("ErrorCode", new List<string> { "SqlError" });
+
+            var failedStorageEventV2Exception =
+                new FailedStorageEventV2Exception(
+                    message: "Failed event storage error occurred, contact support.",
+                    innerException: sqlException,
+                    data: sqlException.Data);
+
+            var expectedEventV2DependencyException =
+                new EventV2DependencyException(
+                    message: "Event dependency error occurred, contact support.",
+                    innerException: failedStorageEventV2Exception);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(DateTimeOffset.MaxValue);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.BulkInsertEventV2sAsync(
+                    It.IsAny<List<EventV2>>(),
+                    It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IEnumerable<EventV2>> bulkRestoreEventV2sTask =
+                this.eventV2Service.BulkRestoreEventV2sAsync(
+                    someEventV2s,
+                        randomCancellationToken);
+
+            EventV2DependencyException actualEventV2DependencyException =
+                await Assert.ThrowsAsync<EventV2DependencyException>(
+                    bulkRestoreEventV2sTask.AsTask);
+
+            // then
+            actualEventV2DependencyException.Should()
+                .BeEquivalentTo(expectedEventV2DependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.BulkInsertEventV2sAsync(
+                    It.IsAny<List<EventV2>>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedEventV2DependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
