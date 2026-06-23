b44e38436caea5557871938cdee096dd9bae36ef
Christo du Toit <christo@dutoit.co.uk>
2026-06-23 22:54:07 +0100

    PROCESSINGS: ListenerEventArchive - Add dependency and timeout exception coverage for batch retrieval

--- EventHighway.Core.Tests.Unit/Services/Processings/ListenerEventArchives/V2/ListenerEventArchiveV2ProcessingServiceTests.Exceptions.RetrieveBatchOfListenerEventArchives.cs
+++ EventHighway.Core.Tests.Unit/Services/Processings/ListenerEventArchives/V2/ListenerEventArchiveV2ProcessingServiceTests.Exceptions.RetrieveBatchOfListenerEventArchives.cs
@@ -16,6 +16,123 @@ namespace EventHighway.Core.Tests.Unit.Services.Processings.ListenerEventArchive
 {
     public partial class ListenerEventArchiveV2ProcessingServiceTests
     {
+        [Theory]
+        [MemberData(nameof(DependencyExceptions))]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveBatchOfListenerEventArchivesIfDependencyExceptionOccursAndLogItAsync(
+            Xeption dependencyException)
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            int skip = GetRandomNumber();
+            int take = GetRandomNumber();
+
+            var expectedException =
+                new ListenerEventArchiveV2ProcessingDependencyException(
+                    message: "Listener event archive dependency error occurred, contact support.",
+                    innerException: dependencyException.InnerException as Xeption);
+
+            this.listenerEventArchiveV2ServiceMock.Setup(service =>
+                service.RetrieveAllListenerEventArchiveV2sAsync(It.IsAny<CancellationToken>()))
+                    .ThrowsAsync(dependencyException);
+
+            // when
+            ValueTask<List<ListenerEventArchiveV2>> retrieveBatchTask =
+                this.listenerEventArchiveV2ProcessingService
+                    .RetrieveBatchOfListenerEventArchiveV2sAsync(
+                        eventAddressId: null,
+                        eventListenerIds: null,
+                        startDate: null,
+                        endDate: null,
+                        skip: skip,
+                        take: take,
+                        cancellationToken: randomCancellationToken);
+
+            ListenerEventArchiveV2ProcessingDependencyException actualException =
+                await Assert.ThrowsAsync<
+                    ListenerEventArchiveV2ProcessingDependencyException>(
+                        retrieveBatchTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedException);
+
+            this.listenerEventArchiveV2ServiceMock.Verify(service =>
+                service.RetrieveAllListenerEventArchiveV2sAsync(It.IsAny<CancellationToken>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(expectedException))),
+                    Times.Once);
+
+            this.listenerEventArchiveV2ServiceMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveBatchOfListenerEventArchivesIfTimeoutOccursAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            int skip = GetRandomNumber();
+            int take = GetRandomNumber();
+
+            var operationCanceledException = new OperationCanceledException();
+
+            var timeoutException =
+                new TimeoutException("The dependency operation timed out.");
+
+            var timeoutListenerEventArchiveV2ProcessingException =
+                new TimeoutListenerEventArchiveV2ProcessingException(
+                    message: "Failed listener event archive processing timeout error occurred, contact support.",
+                    innerException: timeoutException,
+                    data: timeoutException.Data);
+
+            var expectedException =
+                new ListenerEventArchiveV2ProcessingDependencyException(
+                    message: "Listener event archive dependency error occurred, contact support.",
+                    innerException: timeoutListenerEventArchiveV2ProcessingException);
+
+            this.listenerEventArchiveV2ServiceMock.Setup(service =>
+                service.RetrieveAllListenerEventArchiveV2sAsync(It.IsAny<CancellationToken>()))
+                    .ThrowsAsync(operationCanceledException);
+
+            // when
+            ValueTask<List<ListenerEventArchiveV2>> retrieveBatchTask =
+                this.listenerEventArchiveV2ProcessingService
+                    .RetrieveBatchOfListenerEventArchiveV2sAsync(
+                        eventAddressId: null,
+                        eventListenerIds: null,
+                        startDate: null,
+                        endDate: null,
+                        skip: skip,
+                        take: take,
+                        cancellationToken: randomCancellationToken);
+
+            ListenerEventArchiveV2ProcessingDependencyException actualException =
+                await Assert.ThrowsAsync<
+                    ListenerEventArchiveV2ProcessingDependencyException>(
+                        retrieveBatchTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedException);
+
+            this.listenerEventArchiveV2ServiceMock.Verify(service =>
+                service.RetrieveAllListenerEventArchiveV2sAsync(It.IsAny<CancellationToken>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(expectedException))),
+                    Times.Once);
+
+            this.listenerEventArchiveV2ServiceMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
         [Fact]
         public async Task ShouldThrowOperationCanceledExceptionRawWhenCancellationIsRequestedOnRetrieveBatchOfListenerEventArchivesAsync()
         {
