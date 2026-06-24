abded49425e93d5de6d83555984793b1296194d7
Christo du Toit <christo@dutoit.co.uk>
2026-06-24 02:34:27 +0100

    ShouldThrowDependencyValidationExceptionOnReplayIfDependencyValidationErrorOccursAndLogItAsync -> PASS

--- EventHighway.Core/Services/Coordinations/ReplayingEvents/V2/ReplayingEventV2CoordinationService.Exceptions.cs
+++ EventHighway.Core/Services/Coordinations/ReplayingEvents/V2/ReplayingEventV2CoordinationService.Exceptions.cs
@@ -4,6 +4,8 @@
 
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Coordinations.ReplayingEvents.V2.Exceptions;
+using EventHighway.Core.Models.Services.Orchestrations.EventArchives.V2.Exceptions;
+using EventHighway.Core.Models.Services.Orchestrations.RestoringEvents.V2.Exceptions;
 using Xeptions;
 
 namespace EventHighway.Core.Services.Coordinations.ReplayingEvents.V2
@@ -23,6 +25,40 @@ namespace EventHighway.Core.Services.Coordinations.ReplayingEvents.V2
                 throw await CreateAndLogValidationExceptionAsync(
                     invalidReplayingEventV2CoordinationException);
             }
+            catch (EventArchiveV2OrchestrationValidationException eventArchiveV2OrchestrationValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    eventArchiveV2OrchestrationValidationException);
+            }
+            catch (EventArchiveV2OrchestrationDependencyValidationException eventArchiveV2OrchestrationDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    eventArchiveV2OrchestrationDependencyValidationException);
+            }
+            catch (RestoringEventV2OrchestrationValidationException restoringEventV2OrchestrationValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    restoringEventV2OrchestrationValidationException);
+            }
+            catch (RestoringEventV2OrchestrationDependencyValidationException restoringEventV2OrchestrationDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    restoringEventV2OrchestrationDependencyValidationException);
+            }
+        }
+
+        private async ValueTask<ReplayingEventV2CoordinationDependencyValidationException>
+            CreateAndLogDependencyValidationExceptionAsync(Xeption exception)
+        {
+            var replayingEventV2CoordinationDependencyValidationException =
+                new ReplayingEventV2CoordinationDependencyValidationException(
+                    message: "Replaying event validation error occurred, fix the errors and try again.",
+                    innerException: exception.InnerException as Xeption);
+
+            await this.loggingBroker.LogErrorAsync(
+                replayingEventV2CoordinationDependencyValidationException);
+
+            return replayingEventV2CoordinationDependencyValidationException;
         }
 
         private async ValueTask<ReplayingEventV2CoordinationValidationException>
