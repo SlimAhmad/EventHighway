64e433a41ec14c89f33fb873a3d45d8724d342e4
Christo du Toit <christo@dutoit.co.uk>
2026-06-24 01:34:47 +0100

    ShouldThrowDependencyValidationExceptionOnRestoreIfDependencyValidationErrorOccursAndLogItAsync -> PASS

--- EventHighway.Core/Services/Orchestrations/RestoringEvents/V2/RestoringEventV2OrchestrationService.Exceptions.cs
+++ EventHighway.Core/Services/Orchestrations/RestoringEvents/V2/RestoringEventV2OrchestrationService.Exceptions.cs
@@ -3,6 +3,9 @@
 // ----------------------------------------------------------------------------------
 
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Processings.EventListeners.V2.Exceptions;
+using EventHighway.Core.Models.Services.Processings.Events.V2.Exceptions;
+using EventHighway.Core.Models.Services.Processings.ListenerEvents.V2.Exceptions;
 using EventHighway.Core.Models.Services.Orchestrations.RestoringEvents.V2.Exceptions;
 using Xeptions;
 
@@ -23,6 +26,50 @@ namespace EventHighway.Core.Services.Orchestrations.RestoringEvents.V2
                 throw await CreateAndLogValidationExceptionAsync(
                     nullRestoringEventV2OrchestrationException);
             }
+            catch (EventV2ProcessingValidationException eventV2ProcessingValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    eventV2ProcessingValidationException);
+            }
+            catch (EventV2ProcessingDependencyValidationException eventV2ProcessingDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    eventV2ProcessingDependencyValidationException);
+            }
+            catch (ListenerEventV2ProcessingValidationException listenerEventV2ProcessingValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    listenerEventV2ProcessingValidationException);
+            }
+            catch (ListenerEventV2ProcessingDependencyValidationException listenerEventV2ProcessingDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    listenerEventV2ProcessingDependencyValidationException);
+            }
+            catch (EventListenerV2ProcessingValidationException eventListenerV2ProcessingValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    eventListenerV2ProcessingValidationException);
+            }
+            catch (EventListenerV2ProcessingDependencyValidationException eventListenerV2ProcessingDependencyValidationException)
+            {
+                throw await CreateAndLogDependencyValidationExceptionAsync(
+                    eventListenerV2ProcessingDependencyValidationException);
+            }
+        }
+
+        private async ValueTask<RestoringEventV2OrchestrationDependencyValidationException>
+            CreateAndLogDependencyValidationExceptionAsync(Xeption exception)
+        {
+            var restoringEventV2OrchestrationDependencyValidationException =
+                new RestoringEventV2OrchestrationDependencyValidationException(
+                    message: "Restoring event validation error occurred, fix the errors and try again.",
+                    innerException: exception.InnerException as Xeption);
+
+            await this.loggingBroker.LogErrorAsync(
+                restoringEventV2OrchestrationDependencyValidationException);
+
+            return restoringEventV2OrchestrationDependencyValidationException;
         }
 
         private async ValueTask<RestoringEventV2OrchestrationValidationException>
