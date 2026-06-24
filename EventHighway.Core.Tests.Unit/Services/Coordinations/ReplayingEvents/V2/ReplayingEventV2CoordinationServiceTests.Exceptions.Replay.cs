f3f5499ec21ba55718f845bed6464c4e31e93ebf
Christo du Toit <christo@dutoit.co.uk>
2026-06-24 02:44:17 +0100

    ShouldThrowServiceExceptionOnReplayIfServiceErrorOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Coordinations/ReplayingEvents/V2/ReplayingEventV2CoordinationServiceTests.Exceptions.Replay.cs
+++ EventHighway.Core.Tests.Unit/Services/Coordinations/ReplayingEvents/V2/ReplayingEventV2CoordinationServiceTests.Exceptions.Replay.cs
@@ -214,5 +214,74 @@ namespace EventHighway.Core.Tests.Unit.Services.Coordinations.ReplayingEvents.V2
             this.restoringEventV2OrchestrationServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnReplayIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            Guid? eventAddressId = GetRandomId();
+            List<Guid> eventListenerIds = new List<Guid> { GetRandomId() };
+            DateTimeOffset? startDate = GetRandomDateTimeOffset();
+            DateTimeOffset? endDate = startDate.Value.AddDays(GetRandomNumber());
+
+            BatchConfiguration batchConfiguration = CreateBatchConfiguration(GetRandomNumber());
+            var serviceException = new Exception();
+
+            var failedReplayingEventV2CoordinationServiceException =
+                new FailedReplayingEventV2CoordinationServiceException(
+                    message: "Failed replaying event coordination service error occurred, contact support.",
+                    innerException: serviceException,
+                    data: serviceException.Data);
+
+            var expectedException =
+                new ReplayingEventV2CoordinationServiceException(
+                    message: "Replaying event service error occurred, contact support.",
+                    innerException: failedReplayingEventV2CoordinationServiceException);
+
+            this.configurationBrokerMock.Setup(broker =>
+                broker.GetBatchConfiguration())
+                    .Returns(batchConfiguration);
+
+            this.eventArchiveV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveBatchOfListenerEventArchiveV2sAsync(
+                    It.IsAny<Guid?>(), It.IsAny<IEnumerable<Guid>>(),
+                    It.IsAny<DateTimeOffset?>(), It.IsAny<DateTimeOffset?>(),
+                    It.IsAny<int>(), It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                        .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask replayTask =
+                this.replayingEventV2CoordinationService.ReplayEventArchiveV2sAsync(
+                    eventAddressId, eventListenerIds, startDate, endDate, randomCancellationToken);
+
+            ReplayingEventV2CoordinationServiceException actualException =
+                await Assert.ThrowsAsync<ReplayingEventV2CoordinationServiceException>(
+                    replayTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(expectedException);
+
+            this.eventArchiveV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveBatchOfListenerEventArchiveV2sAsync(
+                    It.IsAny<Guid?>(), It.IsAny<IEnumerable<Guid>>(),
+                    It.IsAny<DateTimeOffset?>(), It.IsAny<DateTimeOffset?>(),
+                    It.IsAny<int>(), It.IsAny<int>(), It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(expectedException))),
+                    Times.Once);
+
+            this.configurationBrokerMock.Verify(broker =>
+                broker.GetBatchConfiguration(), Times.Once);
+
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+            this.eventArchiveV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.restoringEventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
