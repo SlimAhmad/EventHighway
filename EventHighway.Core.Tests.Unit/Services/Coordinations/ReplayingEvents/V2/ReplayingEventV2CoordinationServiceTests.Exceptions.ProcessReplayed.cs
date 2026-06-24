b203365afb86e5046ef68b8f359c39e3367ae65a
Christo du Toit <christo@dutoit.co.uk>
2026-06-24 22:10:05 +0100

    ShouldThrowDependencyExceptionOnProcessReplayedIfTimeoutOccursAndLogItAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Coordinations/ReplayingEvents/V2/ReplayingEventV2CoordinationServiceTests.Exceptions.ProcessReplayed.cs
+++ EventHighway.Core.Tests.Unit/Services/Coordinations/ReplayingEvents/V2/ReplayingEventV2CoordinationServiceTests.Exceptions.ProcessReplayed.cs
@@ -197,5 +197,72 @@ namespace EventHighway.Core.Tests.Unit.Services.Coordinations.ReplayingEvents.V2
             this.configurationBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task
+            ShouldThrowDependencyExceptionOnProcessReplayedIfTimeoutOccursAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            int randomTake = GetRandomNumber();
+            var operationCanceledException = new OperationCanceledException();
+
+            var timeoutException =
+                new TimeoutException("The dependency operation timed out.");
+
+            var timeoutReplayingEventV2CoordinationException =
+                new TimeoutReplayingEventV2CoordinationException(
+                    message: "Failed replaying event coordination timeout error occurred, contact support.",
+                    innerException: timeoutException,
+                    data: timeoutException.Data);
+
+            var expectedReplayingEventV2CoordinationDependencyException =
+                new ReplayingEventV2CoordinationDependencyException(
+                    message: "Replaying event dependency error occurred, contact support.",
+                    innerException: timeoutReplayingEventV2CoordinationException);
+
+            this.configurationBrokerMock.Setup(broker =>
+                broker.GetBatchConfiguration())
+                    .Returns(CreateBatchConfiguration(randomTake));
+
+            this.replayingListenerEventV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveBatchOfReplayListenerEventV2sAsync(
+                    randomTake, randomCancellationToken))
+                .ThrowsAsync(operationCanceledException);
+
+            // when
+            ValueTask processReplayedTask =
+                this.replayingEventV2CoordinationService
+                    .ProcessReplayedListenerEventV2sAsync(randomCancellationToken);
+
+            ReplayingEventV2CoordinationDependencyException actualException =
+                await Assert.ThrowsAsync<ReplayingEventV2CoordinationDependencyException>(
+                    processReplayedTask.AsTask);
+
+            // then
+            actualException.Should().BeEquivalentTo(
+                expectedReplayingEventV2CoordinationDependencyException);
+
+            this.configurationBrokerMock.Verify(broker =>
+                broker.GetBatchConfiguration(), Times.Once);
+
+            this.replayingListenerEventV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveBatchOfReplayListenerEventV2sAsync(
+                    randomTake, randomCancellationToken),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedReplayingEventV2CoordinationDependencyException))),
+                Times.Once);
+
+            this.eventArchiveV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.restoringEventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.replayingListenerEventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
