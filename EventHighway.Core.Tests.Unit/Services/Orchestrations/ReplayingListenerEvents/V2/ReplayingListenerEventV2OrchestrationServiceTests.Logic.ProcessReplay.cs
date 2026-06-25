d3fd6fe95e52056ae9b3a37902bb81b8758c1fdb
Christo du Toit <christo@dutoit.co.uk>
2026-06-25 18:07:47 +0100

    CODE RUB: Log delivery exceptions in replay processor catch block
    - Add LogErrorAsync call in the inner catch block of ProcessReplayListenerEventV2Async so delivery failures are observable in logs
    - Add logic test covering the exception path with logging verification

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/ReplayingListenerEvents/V2/ReplayingListenerEventV2OrchestrationServiceTests.Logic.ProcessReplay.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/ReplayingListenerEvents/V2/ReplayingListenerEventV2OrchestrationServiceTests.Logic.ProcessReplay.cs
@@ -196,5 +196,92 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.ReplayingListener
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldProcessReplayListenerEventV2AsErrorWhenDeliveryExceptionThrownAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            ListenerEventV2 inputListenerEventV2 =
+                CreateRandomListenerEventV2WithNavProps();
+
+            inputListenerEventV2.EventListener.PromotedProperties = null;
+
+            DateTimeOffset randomNow = GetRandomDateTimeOffset();
+            var deliveryException = new Exception();
+
+            ListenerEventV2 expectedListenerEventV2 = inputListenerEventV2.DeepClone();
+            expectedListenerEventV2.Status = ListenerEventStatusV2.Error;
+            expectedListenerEventV2.Response = deliveryException.Message;
+            expectedListenerEventV2.UpdatedDate = randomNow;
+
+            ListenerEventV2 returnedListenerEventV2 = expectedListenerEventV2.DeepClone();
+
+            var mockSequence = new MockSequence();
+
+            this.eventCallV2ProcessingServiceMock
+                .InSequence(mockSequence)
+                .Setup(service => service.RunEventCallV2Async(
+                    It.IsAny<EventCallV2>(),
+                    randomCancellationToken))
+                .ThrowsAsync(deliveryException);
+
+            this.loggingBrokerMock
+                .InSequence(mockSequence)
+                .Setup(broker => broker.LogErrorAsync(deliveryException))
+                .Returns(ValueTask.CompletedTask);
+
+            this.dateTimeBrokerMock
+                .InSequence(mockSequence)
+                .Setup(broker => broker.GetDateTimeOffsetAsync())
+                .ReturnsAsync(randomNow);
+
+            this.listenerEventV2ProcessingServiceMock
+                .InSequence(mockSequence)
+                .Setup(service => service.ModifyListenerEventV2Async(
+                    It.Is<ListenerEventV2>(lev =>
+                        lev.Status == ListenerEventStatusV2.Error
+                        && lev.Response == deliveryException.Message
+                        && lev.UpdatedDate == randomNow),
+                    randomCancellationToken))
+                .ReturnsAsync(returnedListenerEventV2);
+
+            // when
+            ListenerEventV2 actualListenerEventV2 =
+                await this.replayingListenerEventV2OrchestrationService
+                    .ProcessReplayListenerEventV2Async(
+                        inputListenerEventV2,
+                        randomCancellationToken);
+
+            // then
+            actualListenerEventV2.Should().BeEquivalentTo(returnedListenerEventV2);
+
+            this.eventCallV2ProcessingServiceMock.Verify(service =>
+                service.RunEventCallV2Async(
+                    It.IsAny<EventCallV2>(),
+                    randomCancellationToken),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(deliveryException),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.listenerEventV2ProcessingServiceMock.Verify(service =>
+                service.ModifyListenerEventV2Async(
+                    It.IsAny<ListenerEventV2>(),
+                    randomCancellationToken),
+                        Times.Once);
+
+            this.eventCallV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.listenerEventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
