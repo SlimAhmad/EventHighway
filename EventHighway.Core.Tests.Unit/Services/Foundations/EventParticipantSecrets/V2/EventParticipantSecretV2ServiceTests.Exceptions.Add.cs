ba76048d22a5f85dd25035ab1fc2cad9e5ea7cbe
Christo du Toit <christo@dutoit.co.uk>
2026-06-26 17:56:21 +0100

    ShouldThrowDependencyExceptionOnAddIfDbUpdateExceptionOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventParticipantSecrets/V2/EventParticipantSecretV2ServiceTests.Exceptions.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventParticipantSecrets/V2/EventParticipantSecretV2ServiceTests.Exceptions.Add.cs
@@ -10,6 +10,7 @@ using EventHighway.Core.Models.Services.Foundations.EventParticipants.V2;
 using EventHighway.Core.Models.Services.Foundations.EventParticipants.V2.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xeptions;
 
@@ -129,6 +130,68 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventParticipantSecr
                 broker.LogErrorAsync(It.Is<Xeption>(
                     actual => actual.SameExceptionAs(
                         expectedEventParticipantSecretV2DependencyValidationException))),
+                            Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventParticipantSecretV2Async(
+                    It.IsAny<EventParticipantSecretV2>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddIfDbUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventParticipantSecretV2 someEventParticipantSecretV2 =
+                CreateRandomEventParticipantSecretV2();
+
+            var dbUpdateException = new DbUpdateException();
+            dbUpdateException.Data.Add("ErrorCode", new List<string> { "DbUpdateError" });
+
+            var failedStorageEventParticipantSecretV2Exception =
+                new FailedStorageEventParticipantSecretV2Exception(
+                    message: "Failed event participant secret storage error occurred, contact support.",
+                    innerException: dbUpdateException,
+                    data: dbUpdateException.Data);
+
+            var expectedEventParticipantSecretV2DependencyException =
+                new EventParticipantSecretV2DependencyException(
+                    message: "Event participant secret dependency error occurred, contact support.",
+                    innerException: failedStorageEventParticipantSecretV2Exception);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ThrowsAsync(dbUpdateException);
+
+            // when
+            ValueTask<EventParticipantSecretV2> addEventParticipantSecretV2Task =
+                this.eventParticipantSecretV2Service.AddEventParticipantSecretV2Async(
+                    someEventParticipantSecretV2, randomCancellationToken);
+
+            EventParticipantSecretV2DependencyException actualEventParticipantSecretV2DependencyException =
+                await Assert.ThrowsAsync<EventParticipantSecretV2DependencyException>(
+                    addEventParticipantSecretV2Task.AsTask);
+
+            // then
+            actualEventParticipantSecretV2DependencyException.Should()
+                .BeEquivalentTo(expectedEventParticipantSecretV2DependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is<Xeption>(
+                    actual => actual.SameExceptionAs(
+                        expectedEventParticipantSecretV2DependencyException))),
                             Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
