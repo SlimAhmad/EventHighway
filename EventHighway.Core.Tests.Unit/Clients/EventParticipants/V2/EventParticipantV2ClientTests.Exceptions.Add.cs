9eb31e8c1161cd436f5c44f55e03a97210469cb0
Christo du Toit <christo@dutoit.co.uk>
2026-06-26 14:42:14 +0100

    ShouldThrowValidationExceptionOnAddIfDependencyValidationExceptionOccursAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Clients/EventParticipants/V2/EventParticipantV2ClientTests.Exceptions.Add.cs
+++ EventHighway.Core.Tests.Unit/Clients/EventParticipants/V2/EventParticipantV2ClientTests.Exceptions.Add.cs
@@ -66,5 +66,57 @@ namespace EventHighway.Core.Tests.Unit.Clients.EventParticipants.V2
 
             this.eventParticipantV2ServiceMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task
+            ShouldThrowValidationExceptionOnAddIfDependencyValidationExceptionOccursAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventParticipantV2 someEventParticipantV2 = CreateRandomEventParticipantV2();
+            var someInnerException = new Xeption(message: GetRandomString());
+
+            var eventParticipantV2DependencyValidationException =
+                new EventParticipantV2DependencyValidationException(
+                    message: "Event participant validation error occurred, fix the errors and try again.",
+                    innerException: someInnerException);
+
+            var expectedEventParticipantV2ClientValidationException =
+                new EventParticipantV2ClientValidationException(
+                    message: "Event participant client validation error occurred, fix the errors and try again.",
+                    innerException: someInnerException,
+                    data: someInnerException.Data);
+
+            this.eventParticipantV2ServiceMock.Setup(service =>
+                service.AddEventParticipantV2Async(
+                    someEventParticipantV2,
+                    randomCancellationToken))
+                        .ThrowsAsync(eventParticipantV2DependencyValidationException);
+
+            // when
+            ValueTask<EventParticipantV2> addEventParticipantV2Task =
+                this.eventParticipantV2Client.AddEventParticipantV2Async(
+                    someEventParticipantV2,
+                    randomCancellationToken);
+
+            EventParticipantV2ClientValidationException
+                actualEventParticipantV2ClientValidationException =
+                    await Assert.ThrowsAsync<EventParticipantV2ClientValidationException>(
+                        addEventParticipantV2Task.AsTask);
+
+            // then
+            actualEventParticipantV2ClientValidationException.Should()
+                .BeEquivalentTo(expectedEventParticipantV2ClientValidationException);
+
+            this.eventParticipantV2ServiceMock.Verify(service =>
+                service.AddEventParticipantV2Async(
+                    someEventParticipantV2,
+                    randomCancellationToken),
+                        Times.Once);
+
+            this.eventParticipantV2ServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
