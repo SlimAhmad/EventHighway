d29713f309f1e25a5867464a47d1ccb36cf21fdb
Christo du Toit <christo@dutoit.co.uk>
2026-06-26 18:11:26 +0100

    ShouldThrowDependencyExceptionOnAddIfTimeoutOccursAndLogItAsync -> FAIL ShouldThrowOperationCanceledExceptionRawWhenCancellationIsRequestedOnAddAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/EventParticipantSecrets/V2/EventParticipantSecretV2ServiceTests.Exceptions.Add.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/EventParticipantSecrets/V2/EventParticipantSecretV2ServiceTests.Exceptions.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -203,6 +204,167 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.EventParticipantSecr
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnAddIfExceptionOccursAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            EventParticipantSecretV2 someEventParticipantSecretV2 =
+                CreateRandomEventParticipantSecretV2();
+
+            var serviceException = new Exception();
+            serviceException.Data.Add("ErrorCode", new List<string> { "ServiceError" });
+
+            var failedEventParticipantSecretV2ServiceException =
+                new FailedEventParticipantSecretV2ServiceException(
+                    message: "Failed event participant secret service error occurred, contact support.",
+                    innerException: serviceException,
+                    data: serviceException.Data);
+
+            var expectedEventParticipantSecretV2ServiceException =
+                new EventParticipantSecretV2ServiceException(
+                    message: "Event participant secret service error occurred, contact support.",
+                    innerException: failedEventParticipantSecretV2ServiceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<EventParticipantSecretV2> addEventParticipantSecretV2Task =
+                this.eventParticipantSecretV2Service.AddEventParticipantSecretV2Async(
+                    someEventParticipantSecretV2, randomCancellationToken);
+
+            EventParticipantSecretV2ServiceException actualEventParticipantSecretV2ServiceException =
+                await Assert.ThrowsAsync<EventParticipantSecretV2ServiceException>(
+                    addEventParticipantSecretV2Task.AsTask);
+
+            // then
+            actualEventParticipantSecretV2ServiceException.Should()
+                .BeEquivalentTo(expectedEventParticipantSecretV2ServiceException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is<Xeption>(
+                    actual => actual.SameExceptionAs(
+                        expectedEventParticipantSecretV2ServiceException))),
+                            Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventParticipantSecretV2Async(
+                    It.IsAny<EventParticipantSecretV2>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddIfTimeoutOccursAndLogItAsync()
+        {
+            // given
+            EventParticipantSecretV2 someEventParticipantSecretV2 =
+                CreateRandomEventParticipantSecretV2();
+
+            var operationCanceledException = new OperationCanceledException();
+
+            var timeoutException =
+                new TimeoutException("The dependency operation timed out.");
+
+            var timeoutEventParticipantSecretV2Exception =
+                new TimeoutEventParticipantSecretV2Exception(
+                    message: "Failed event participant secret timeout error occurred, contact support.",
+                    innerException: timeoutException,
+                    data: timeoutException.Data);
+
+            var expectedEventParticipantSecretV2DependencyException =
+                new EventParticipantSecretV2DependencyException(
+                    message: "Event participant secret dependency error occurred, contact support.",
+                    innerException: timeoutEventParticipantSecretV2Exception);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ThrowsAsync(operationCanceledException);
+
+            // when
+            ValueTask<EventParticipantSecretV2> addEventParticipantSecretV2Task =
+                this.eventParticipantSecretV2Service.AddEventParticipantSecretV2Async(
+                    someEventParticipantSecretV2, TestContext.Current.CancellationToken);
+
+            EventParticipantSecretV2DependencyException actualEventParticipantSecretV2DependencyException =
+                await Assert.ThrowsAsync<EventParticipantSecretV2DependencyException>(
+                    addEventParticipantSecretV2Task.AsTask);
+
+            // then
+            actualEventParticipantSecretV2DependencyException.Should().BeEquivalentTo(
+                expectedEventParticipantSecretV2DependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is<Xeption>(
+                    actual => actual.SameExceptionAs(
+                        expectedEventParticipantSecretV2DependencyException))),
+                            Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertEventParticipantSecretV2Async(
+                    It.IsAny<EventParticipantSecretV2>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowOperationCanceledExceptionRawWhenCancellationIsRequestedOnAddAsync()
+        {
+            // given
+            EventParticipantSecretV2 someEventParticipantSecretV2 =
+                CreateRandomEventParticipantSecretV2();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.Cancel();
+            CancellationToken cancelledToken = cancellationTokenSource.Token;
+
+            // when
+            ValueTask<EventParticipantSecretV2> addEventParticipantSecretV2Task =
+                this.eventParticipantSecretV2Service.AddEventParticipantSecretV2Async(
+                    someEventParticipantSecretV2, cancelledToken);
+
+            // then
+            OperationCanceledException actualException =
+                await Assert.ThrowsAsync<OperationCanceledException>(
+                    addEventParticipantSecretV2Task.AsTask);
+
+            actualException.Should().NotBeOfType<EventParticipantSecretV2DependencyException>();
+            actualException.Should().NotBeOfType<EventParticipantSecretV2ServiceException>();
+            actualException.CancellationToken.IsCancellationRequested.Should().BeTrue();
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.IsAny<Xeptions.Xeption>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.IsAny<Xeptions.Xeption>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
