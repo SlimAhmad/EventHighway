12e58be545e1f2068fe2ca4cd8cb24cb62577354
Christo du Toit <christo@dutoit.co.uk>
2026-06-26 01:18:57 +0100

    BROKERS: EventParticipantV2 and EventParticipantSecretV2 CRUD storage broker methods

--- EventHighway.Core/Brokers/Storages/IStorageBroker.EventParticipantV2s.cs
+++ EventHighway.Core/Brokers/Storages/IStorageBroker.EventParticipantV2s.cs
@@ -2,6 +2,10 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using EventHighway.Core.Models.Services.Foundations.EventParticipants.V2;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +14,24 @@ namespace EventHighway.Core.Brokers.Storages
     internal partial interface IStorageBroker
     {
         DbSet<EventParticipantV2> EventParticipantV2s { get; set; }
+
+        ValueTask<EventParticipantV2> InsertEventParticipantV2Async(
+            EventParticipantV2 eventParticipantV2,
+            CancellationToken cancellationToken = default);
+
+        ValueTask<IQueryable<EventParticipantV2>> SelectAllEventParticipantV2sAsync(
+            CancellationToken cancellationToken = default);
+
+        ValueTask<EventParticipantV2> SelectEventParticipantV2ByIdAsync(
+            Guid eventParticipantV2Id,
+            CancellationToken cancellationToken = default);
+
+        ValueTask<EventParticipantV2> UpdateEventParticipantV2Async(
+            EventParticipantV2 eventParticipantV2,
+            CancellationToken cancellationToken = default);
+
+        ValueTask<EventParticipantV2> DeleteEventParticipantV2Async(
+            EventParticipantV2 eventParticipantV2,
+            CancellationToken cancellationToken = default);
     }
 }
--- EventHighway.Core/Brokers/Storages/StorageBroker.EventParticipantSecretV2s.cs
+++ EventHighway.Core/Brokers/Storages/StorageBroker.EventParticipantSecretV2s.cs
@@ -2,6 +2,10 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using EventHighway.Core.Models.Services.Foundations.EventParticipants.V2;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +14,31 @@ namespace EventHighway.Core.Brokers.Storages
     internal partial class StorageBroker
     {
         public DbSet<EventParticipantSecretV2> EventParticipantSecretV2s { get; set; }
+
+        public async ValueTask<EventParticipantSecretV2> InsertEventParticipantSecretV2Async(
+            EventParticipantSecretV2 eventParticipantSecretV2,
+            CancellationToken cancellationToken = default) =>
+            await InsertAsync(eventParticipantSecretV2, cancellationToken);
+
+        public async ValueTask<IQueryable<EventParticipantSecretV2>> SelectAllEventParticipantSecretV2sAsync(
+            CancellationToken cancellationToken = default) =>
+            await SelectAllAsync<EventParticipantSecretV2>(cancellationToken);
+
+        public async ValueTask<EventParticipantSecretV2> SelectEventParticipantSecretV2ByIdAsync(
+            Guid eventParticipantSecretV2Id,
+            CancellationToken cancellationToken = default) =>
+            await SelectAsync<EventParticipantSecretV2>(
+                new object[] { eventParticipantSecretV2Id },
+                cancellationToken);
+
+        public async ValueTask<EventParticipantSecretV2> UpdateEventParticipantSecretV2Async(
+            EventParticipantSecretV2 eventParticipantSecretV2,
+            CancellationToken cancellationToken = default) =>
+            await UpdateAsync(eventParticipantSecretV2, cancellationToken);
+
+        public async ValueTask<EventParticipantSecretV2> DeleteEventParticipantSecretV2Async(
+            EventParticipantSecretV2 eventParticipantSecretV2,
+            CancellationToken cancellationToken = default) =>
+            await DeleteAsync(eventParticipantSecretV2, cancellationToken);
     }
 }
