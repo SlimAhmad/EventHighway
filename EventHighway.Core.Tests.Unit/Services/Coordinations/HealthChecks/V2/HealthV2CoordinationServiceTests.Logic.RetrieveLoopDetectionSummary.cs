d1f0a8c85469f844846352d4a00dbf84746c0e83
Christo du Toit <christo@dutoit.co.uk>
2026-06-27 01:36:14 +0100

    COORDINATIONS: Add failing loop-by-participant split test and LoopDetailV2 participant fields

--- EventHighway.Core.Tests.Unit/Services/Coordinations/HealthChecks/V2/HealthV2CoordinationServiceTests.Logic.RetrieveLoopDetectionSummary.cs
+++ EventHighway.Core.Tests.Unit/Services/Coordinations/HealthChecks/V2/HealthV2CoordinationServiceTests.Logic.RetrieveLoopDetectionSummary.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Coordinations.HealthChecks.V2;
 using EventHighway.Core.Models.Services.Foundations.EventAddresses.V2;
+using EventHighway.Core.Models.Services.Foundations.EventParticipants.V2;
 using EventHighway.Core.Models.Services.Foundations.Events.V2;
 using EventHighway.Core.Models.Services.Foundations.EventsArchives.V2;
 using FluentAssertions;
@@ -99,6 +100,104 @@ namespace EventHighway.Core.Tests.Unit.Services.Coordinations.HealthChecks.V2
             detailB.AddressName.Should().Be(addressBName);
             detailB.ActiveQuarantined.Should().Be(1);
             detailB.ArchivedQuarantined.Should().Be(0);
+            detailB.InWindow.Should().Be(1);
+            detailB.MostRecentDetection.Should().Be(windowStart.AddHours(3));
+
+            this.eventV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventAddressV2sAsync(randomCancellationToken), Times.Once);
+
+            this.eventV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventV2sAsync(randomCancellationToken), Times.Once);
+
+            this.eventArchiveV2OrchestrationServiceMock.Verify(service =>
+                service.RetrieveAllEventArchiveV2sAsync(randomCancellationToken), Times.Once);
+
+            this.configurationBrokerMock.Verify(broker =>
+                broker.GetHealthConfiguration(), Times.Once);
+
+            this.eventV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.eventListenerV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.eventArchiveV2OrchestrationServiceMock.VerifyNoOtherCalls();
+            this.configurationBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldSplitLoopDetectionSummaryV2ByParticipantForWindowAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            var windowStart = new DateTimeOffset(2026, 6, 24, 0, 0, 0, TimeSpan.Zero);
+            Guid addressId = Guid.NewGuid();
+            string addressName = GetRandomString();
+
+            Guid participantAId = Guid.NewGuid();
+            Guid participantBId = Guid.NewGuid();
+            string participantAName = GetRandomString();
+            string participantBName = GetRandomString();
+
+            EventParticipantV2 participantA = CreateEventParticipantV2(
+                participantAId, participantAName, GetRandomString(), GetRandomString(), true);
+
+            EventParticipantV2 participantB = CreateEventParticipantV2(
+                participantBId, participantBName, GetRandomString(), GetRandomString(), true);
+
+            var addresses = new[]
+            {
+                CreateEventAddressV2(addressId, addressName, GetRandomString())
+            }.AsQueryable();
+
+            var events = new[]
+            {
+                CreateEventV2ForParticipant(Guid.NewGuid(), addressId, participantAId, participantA,
+                    windowStart.AddHours(1), "a1", EventStatusV2.Quarantined),
+                CreateEventV2ForParticipant(Guid.NewGuid(), addressId, participantAId, participantA,
+                    windowStart.AddHours(2), "a2", EventStatusV2.Quarantined),
+                CreateEventV2ForParticipant(Guid.NewGuid(), addressId, participantBId, participantB,
+                    windowStart.AddHours(3), "b1", EventStatusV2.Quarantined)
+            }.AsQueryable();
+
+            var emptyArchivedEvents = Array.Empty<EventArchiveV2>().AsQueryable();
+
+            this.eventV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllEventAddressV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(addresses);
+
+            this.eventV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllEventV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(events);
+
+            this.eventArchiveV2OrchestrationServiceMock.Setup(service =>
+                service.RetrieveAllEventArchiveV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(emptyArchivedEvents);
+
+            // when
+            LoopDetectionSummaryV2 actualSummary =
+                await this.healthV2CoordinationService
+                    .RetrieveLoopDetectionSummaryV2Async(
+                        TrafficPeriodV2.Day, windowStart, randomCancellationToken);
+
+            // then
+            actualSummary.ByAddress.Should().HaveCount(2);
+
+            LoopDetailV2 detailA =
+                actualSummary.ByAddress.Single(d => d.ParticipantId == participantAId);
+
+            detailA.EventAddressId.Should().Be(addressId);
+            detailA.AddressName.Should().Be(addressName);
+            detailA.ParticipantName.Should().Be(participantAName);
+            detailA.ActiveQuarantined.Should().Be(2);
+            detailA.ArchivedQuarantined.Should().Be(0);
+            detailA.InWindow.Should().Be(2);
+            detailA.MostRecentDetection.Should().Be(windowStart.AddHours(2));
+
+            LoopDetailV2 detailB =
+                actualSummary.ByAddress.Single(d => d.ParticipantId == participantBId);
+
+            detailB.ParticipantName.Should().Be(participantBName);
+            detailB.ActiveQuarantined.Should().Be(1);
             detailB.InWindow.Should().Be(1);
             detailB.MostRecentDetection.Should().Be(windowStart.AddHours(3));
 
