96b2934b76612fe2e2354e89bce37f7f581439c6
Christo du Toit <christo@dutoit.co.uk>
2026-06-28 20:55:00 +0100

    ShouldRenderButtonWithColorTypeAndChildContent -> FAIL

--- EventHighway.Portal.Web.Tests.Unit/Components/CoreUI/ButtonComponentTests.Render.cs
+++ EventHighway.Portal.Web.Tests.Unit/Components/CoreUI/ButtonComponentTests.Render.cs
@@ -22,5 +22,27 @@ namespace EventHighway.Portal.Web.Tests.Unit.Components.CoreUI
             renderedButton.Instance.Type.Should().Be("button");
             renderedButton.Instance.Disabled.Should().BeFalse();
         }
+
+        [Fact]
+        public void ShouldRenderButtonWithColorTypeAndChildContent()
+        {
+            // given
+            string randomLabel = GetRandomString();
+
+            // when
+            IRenderedComponent<Button> renderedButton =
+                Render<Button>(parameters =>
+                    parameters
+                        .Add(button => button.Color, "success")
+                        .Add(button => button.Type, "submit")
+                        .AddChildContent(randomLabel));
+
+            // then
+            var buttonElement = renderedButton.Find("button");
+
+            buttonElement.ClassList.Should().Contain(new[] { "btn", "btn-success" });
+            buttonElement.GetAttribute("type").Should().Be("submit");
+            buttonElement.TextContent.Trim().Should().Be(randomLabel);
+        }
     }
 }
