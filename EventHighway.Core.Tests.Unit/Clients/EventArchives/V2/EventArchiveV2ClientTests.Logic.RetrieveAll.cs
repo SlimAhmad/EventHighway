c30f27f58eaa1627a9572472f97cf68b4be79a19
Christo du Toit <christo@dutoit.co.uk>
2026-06-28 19:24:04 +0100

    TESTS: Add failing EventArchiveV2 client retrieve all logic test

---
+++ EventHighway.Core.Tests.Unit/Clients/EventArchives/V2/EventArchiveV2ClientTests.Logic.RetrieveAll.cs
@@ -0,0 +1,53 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V2;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Clients.EventArchives.V2
+{
+    public partial class EventArchiveV2ClientTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveAllEventArchiveV2sAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            IQueryable<EventArchiveV2> randomEventArchiveV2s =
+                CreateRandomEventArchiveV2s();
+
+            IQueryable<EventArchiveV2> retrievedEventArchiveV2s =
+                randomEventArchiveV2s;
+
+            IQueryable<EventArchiveV2> expectedEventArchiveV2s =
+                retrievedEventArchiveV2s.DeepClone();
+
+            this.eventArchiveV2ServiceMock.Setup(service =>
+                service.RetrieveAllEventArchiveV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(retrievedEventArchiveV2s);
+
+            // when
+            IQueryable<EventArchiveV2> actualEventArchiveV2s =
+                await this.eventArchiveV2Client
+                    .RetrieveAllEventArchiveV2sAsync(randomCancellationToken);
+
+            // then
+            actualEventArchiveV2s.Should().BeEquivalentTo(
+                expectedEventArchiveV2s);
+
+            this.eventArchiveV2ServiceMock.Verify(service =>
+                service.RetrieveAllEventArchiveV2sAsync(randomCancellationToken),
+                    Times.Once);
+
+            this.eventArchiveV2ServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ EventHighway.Core/Clients/EventArchives/V2/EventArchiveV2Client.cs
@@ -0,0 +1,39 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V2;
+using EventHighway.Core.Services.Foundations.EventArchives.V2;
+
+namespace EventHighway.Core.Clients.EventArchives.V2
+{
+    /// <summary>
+    /// Represents the V2 event archive client implementation, exposing read operations over
+    /// archived events while managing foundation service exceptions.
+    /// </summary>
+    internal class EventArchiveV2Client : IEventArchiveV2Client
+    {
+        private readonly IEventArchiveV2Service eventArchiveV2Service;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventArchiveV2Client"/> class with the
+        /// specified event archive service.
+        /// </summary>
+        /// <param name="eventArchiveV2Service">The foundation service for archived events.</param>
+        public EventArchiveV2Client(IEventArchiveV2Service eventArchiveV2Service) =>
+            this.eventArchiveV2Service = eventArchiveV2Service;
+
+        public ValueTask<IQueryable<EventArchiveV2>> RetrieveAllEventArchiveV2sAsync(
+            CancellationToken cancellationToken = default) =>
+            throw new NotImplementedException();
+
+        public ValueTask<EventArchiveV2> RetrieveEventArchiveV2ByIdAsync(
+            Guid eventArchiveV2Id,
+            CancellationToken cancellationToken = default) =>
+            throw new NotImplementedException();
+    }
+}
