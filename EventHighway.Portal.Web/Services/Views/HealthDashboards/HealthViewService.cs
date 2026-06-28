79ac72d1555d152873635f1bc91d746f5cde1176
Christo du Toit <christo@dutoit.co.uk>
2026-06-28 22:17:11 +0100

    ShouldRetrieveHealthRagTilesAsync -> PASS

--- EventHighway.Portal.Web/Services/Views/HealthDashboards/HealthViewService.cs
+++ EventHighway.Portal.Web/Services/Views/HealthDashboards/HealthViewService.cs
@@ -2,12 +2,14 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Coordinations.HealthChecks.V2;
 using EventHighway.Portal.Web.Brokers.EventHighways;
 using EventHighway.Portal.Web.Brokers.Loggings;
+using EventHighway.Portal.Web.Components.CoreUI;
 using EventHighway.Portal.Web.Models.Views.HealthDashboards;
 
 namespace EventHighway.Portal.Web.Services.Views.HealthDashboards
@@ -25,8 +27,34 @@ namespace EventHighway.Portal.Web.Services.Views.HealthDashboards
             this.loggingBroker = loggingBroker;
         }
 
-        public ValueTask<List<HealthRagTile>> RetrieveHealthRagTilesAsync(
-            CancellationToken cancellationToken = default) =>
-            throw new NotImplementedException();
+        public async ValueTask<List<HealthRagTile>> RetrieveHealthRagTilesAsync(
+            CancellationToken cancellationToken = default)
+        {
+            IEnumerable<HealthCheckItemV2> healthCheckItems =
+                await this.eventHighwayBroker.RetrieveHealthRagStatusV2Async(
+                    cancellationToken);
+
+            return healthCheckItems.Select(AsRagTile).ToList();
+        }
+
+        private static HealthRagTile AsRagTile(HealthCheckItemV2 healthCheckItem) =>
+            new HealthRagTile
+            {
+                Grouping = healthCheckItem.Grouping,
+                Label = healthCheckItem.Item,
+                Value = healthCheckItem.Value,
+                Description = healthCheckItem.Description,
+                StatusCode = healthCheckItem.StatusCode,
+                Variant = AsVariant(healthCheckItem.Status)
+            };
+
+        private static StatTileVariant AsVariant(string status) =>
+            status switch
+            {
+                "Green" => StatTileVariant.Green,
+                "Amber" => StatTileVariant.Amber,
+                "Red" => StatTileVariant.Red,
+                _ => StatTileVariant.Na
+            };
     }
 }
