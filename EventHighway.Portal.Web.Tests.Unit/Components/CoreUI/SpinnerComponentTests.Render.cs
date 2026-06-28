cf3ac0c222670ad2e239dfff1234788ad9771e41
Christo du Toit <christo@dutoit.co.uk>
2026-06-28 22:05:34 +0100

    ShouldInitializeComponent -> PASS

---
+++ EventHighway.Portal.Web.Tests.Unit/Components/CoreUI/SpinnerComponentTests.Render.cs
@@ -0,0 +1,24 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Bunit;
+using EventHighway.Portal.Web.Components.CoreUI;
+using FluentAssertions;
+
+namespace EventHighway.Portal.Web.Tests.Unit.Components.CoreUI
+{
+    public partial class SpinnerComponentTests
+    {
+        [Fact]
+        public void ShouldInitializeComponent()
+        {
+            // given . when
+            IRenderedComponent<Spinner> renderedSpinner = Render<Spinner>();
+
+            // then
+            renderedSpinner.Instance.Visible.Should().BeTrue();
+            renderedSpinner.Find("div.spinner-border").Should().NotBeNull();
+        }
+    }
+}
