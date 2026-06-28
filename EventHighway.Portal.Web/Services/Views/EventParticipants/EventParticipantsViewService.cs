5cc0f3f9bdd0db46559fb1b9438618e630a48233
Christo du Toit <christo@dutoit.co.uk>
2026-06-28 23:00:24 +0100

    ShouldRetrieveAllParticipantsAsync -> PASS

--- EventHighway.Portal.Web/Services/Views/EventParticipants/EventParticipantsViewService.cs
+++ EventHighway.Portal.Web/Services/Views/EventParticipants/EventParticipantsViewService.cs
@@ -2,10 +2,11 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventParticipants.V2;
 using EventHighway.Portal.Web.Brokers.EventHighways;
 using EventHighway.Portal.Web.Brokers.Loggings;
 using EventHighway.Portal.Web.Models.Views.EventParticipants;
@@ -25,8 +26,27 @@ namespace EventHighway.Portal.Web.Services.Views.EventParticipants
             this.loggingBroker = loggingBroker;
         }
 
-        public ValueTask<List<EventParticipantView>> RetrieveAllParticipantsAsync(
-            CancellationToken cancellationToken = default) =>
-            throw new NotImplementedException();
+        public async ValueTask<List<EventParticipantView>> RetrieveAllParticipantsAsync(
+            CancellationToken cancellationToken = default)
+        {
+            IEnumerable<EventParticipantV2> participants =
+                await this.eventHighwayBroker.RetrieveAllEventParticipantV2sAsync(
+                    cancellationToken);
+
+            return participants.Select(AsView).ToList();
+        }
+
+        private static EventParticipantView AsView(EventParticipantV2 participant) =>
+            new EventParticipantView
+            {
+                Id = participant.Id,
+                Name = participant.Name,
+                Description = participant.Description,
+                ContactEmail = participant.ContactEmail,
+                ContactPhone = participant.ContactPhone,
+                IsActive = participant.IsActive,
+                ActiveFrom = participant.ActiveFrom,
+                ActiveTo = participant.ActiveTo
+            };
     }
 }
