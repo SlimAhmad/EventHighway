191467eadf3e5ac9c40d328e4b229b29ea1d4fa4
Christo du Toit <christo@dutoit.co.uk>
2026-06-28 22:03:44 +0100

    ShouldRenderTitleBodyAndFooter -> FAIL

--- EventHighway.Portal.Web.Tests.Unit/Components/CoreUI/CardComponentTests.Render.cs
+++ EventHighway.Portal.Web.Tests.Unit/Components/CoreUI/CardComponentTests.Render.cs
@@ -20,5 +20,26 @@ namespace EventHighway.Portal.Web.Tests.Unit.Components.CoreUI
             renderedCard.Find("div.card").Should().NotBeNull();
             renderedCard.Instance.Title.Should().BeNull();
         }
+
+        [Fact]
+        public void ShouldRenderTitleBodyAndFooter()
+        {
+            // given
+            string randomTitle = GetRandomString();
+            string randomBody = GetRandomString();
+            string randomFooter = GetRandomString();
+
+            // when
+            IRenderedComponent<Card> renderedCard =
+                Render<Card>(parameters => parameters
+                    .Add(card => card.Title, randomTitle)
+                    .AddChildContent(randomBody)
+                    .Add(card => card.FooterContent, randomFooter));
+
+            // then
+            renderedCard.Find("div.card-header").TextContent.Should().Contain(randomTitle);
+            renderedCard.Find("div.card-body").TextContent.Should().Contain(randomBody);
+            renderedCard.Find("div.card-footer").TextContent.Should().Contain(randomFooter);
+        }
     }
 }
