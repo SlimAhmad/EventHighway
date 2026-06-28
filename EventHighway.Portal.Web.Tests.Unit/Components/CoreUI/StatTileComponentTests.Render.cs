156207e0fb204c24ddcecc0e6551828b12be979b
Christo du Toit <christo@dutoit.co.uk>
2026-06-28 22:11:28 +0100

    ShouldRenderValueAndLabel -> PASS

--- EventHighway.Portal.Web.Tests.Unit/Components/CoreUI/StatTileComponentTests.Render.cs
+++ EventHighway.Portal.Web.Tests.Unit/Components/CoreUI/StatTileComponentTests.Render.cs
@@ -33,5 +33,23 @@ namespace EventHighway.Portal.Web.Tests.Unit.Components.CoreUI
             // then
             renderedStatTile.Find("div.stat-tile").ClassList.Should().Contain(expectedCssClass);
         }
+
+        [Fact]
+        public void ShouldRenderValueAndLabel()
+        {
+            // given
+            string randomValue = GetRandomString();
+            string randomLabel = GetRandomString();
+
+            // when
+            IRenderedComponent<StatTile> renderedStatTile =
+                Render<StatTile>(parameters => parameters
+                    .Add(statTile => statTile.Value, randomValue)
+                    .Add(statTile => statTile.Label, randomLabel));
+
+            // then
+            renderedStatTile.Markup.Should().Contain(randomValue);
+            renderedStatTile.Markup.Should().Contain(randomLabel);
+        }
     }
 }
