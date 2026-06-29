90fbd96ba893a3b783e6ec4d23e847cd61e6d58e
Christo du Toit <christo@dutoit.co.uk>
2026-06-29 08:55:17 +0100

    BROKERS: Add Health Traffic Address Loop Duplicate Retry Participant Methods

--- EventHighway.Portal.Web/Brokers/EventHighways/EventHighwayBroker.Health.cs
+++ EventHighway.Portal.Web/Brokers/EventHighways/EventHighwayBroker.Health.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,5 +16,48 @@ namespace EventHighway.Portal.Web.Brokers.EventHighways
             CancellationToken cancellationToken = default) =>
             await this.clientV2.HealthStatusClientV2
                 .RetrieveHealthRagStatusV2Async(cancellationToken);
+
+        public async ValueTask<TrafficSnapshotV2> RetrieveTrafficSnapshotV2Async(
+            TrafficPeriodV2 period,
+            DateTimeOffset windowStart,
+            CancellationToken cancellationToken = default) =>
+            await this.clientV2.HealthTrafficClientV2
+                .RetrieveTrafficSnapshotV2Async(period, windowStart, cancellationToken);
+
+        public async ValueTask<IEnumerable<EventAddressSummaryV2>>
+            RetrieveEventAddressSummaryV2Async(
+                TrafficPeriodV2 period,
+                DateTimeOffset windowStart,
+                CancellationToken cancellationToken = default) =>
+            await this.clientV2.HealthAddressClientV2
+                .RetrieveEventAddressSummaryV2Async(period, windowStart, cancellationToken);
+
+        public async ValueTask<LoopDetectionSummaryV2> RetrieveLoopDetectionSummaryV2Async(
+            TrafficPeriodV2 period,
+            DateTimeOffset windowStart,
+            CancellationToken cancellationToken = default) =>
+            await this.clientV2.HealthLoopClientV2
+                .RetrieveLoopDetectionSummaryV2Async(period, windowStart, cancellationToken);
+
+        public async ValueTask<DuplicateDetectionSummaryV2>
+            RetrieveDuplicateDetectionSummaryV2Async(
+                TrafficPeriodV2 period,
+                DateTimeOffset windowStart,
+                CancellationToken cancellationToken = default) =>
+            await this.clientV2.HealthDuplicateClientV2
+                .RetrieveDuplicateDetectionSummaryV2Async(period, windowStart, cancellationToken);
+
+        public async ValueTask<RetryHealthSummaryV2> RetrieveRetryHealthV2Async(
+            CancellationToken cancellationToken = default) =>
+            await this.clientV2.HealthRetryClientV2
+                .RetrieveRetryHealthV2Async(cancellationToken);
+
+        public async ValueTask<IEnumerable<ParticipantSummaryV2>>
+            RetrieveParticipantSummaryV2Async(
+                TrafficPeriodV2 period,
+                DateTimeOffset windowStart,
+                CancellationToken cancellationToken = default) =>
+            await this.clientV2.HealthParticipantClientV2
+                .RetrieveParticipantSummaryV2Async(period, windowStart, cancellationToken);
     }
 }
--- EventHighway.Portal.Web/Brokers/EventHighways/IEventHighwayBroker.Health.cs
+++ EventHighway.Portal.Web/Brokers/EventHighways/IEventHighwayBroker.Health.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,6 +13,34 @@ namespace EventHighway.Portal.Web.Brokers.EventHighways
     public partial interface IEventHighwayBroker
     {
         ValueTask<IEnumerable<HealthCheckItemV2>> RetrieveHealthRagStatusV2Async(
+            CancellationToken cancellationToken = default);
+
+        ValueTask<TrafficSnapshotV2> RetrieveTrafficSnapshotV2Async(
+            TrafficPeriodV2 period,
+            DateTimeOffset windowStart,
+            CancellationToken cancellationToken = default);
+
+        ValueTask<IEnumerable<EventAddressSummaryV2>> RetrieveEventAddressSummaryV2Async(
+            TrafficPeriodV2 period,
+            DateTimeOffset windowStart,
+            CancellationToken cancellationToken = default);
+
+        ValueTask<LoopDetectionSummaryV2> RetrieveLoopDetectionSummaryV2Async(
+            TrafficPeriodV2 period,
+            DateTimeOffset windowStart,
+            CancellationToken cancellationToken = default);
+
+        ValueTask<DuplicateDetectionSummaryV2> RetrieveDuplicateDetectionSummaryV2Async(
+            TrafficPeriodV2 period,
+            DateTimeOffset windowStart,
+            CancellationToken cancellationToken = default);
+
+        ValueTask<RetryHealthSummaryV2> RetrieveRetryHealthV2Async(
+            CancellationToken cancellationToken = default);
+
+        ValueTask<IEnumerable<ParticipantSummaryV2>> RetrieveParticipantSummaryV2Async(
+            TrafficPeriodV2 period,
+            DateTimeOffset windowStart,
             CancellationToken cancellationToken = default);
     }
 }
