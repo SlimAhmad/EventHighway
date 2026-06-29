0bff41fb5871e03850662539aa953a1a35d7fec5
Christo du Toit <christo@dutoit.co.uk>
2026-06-29 23:47:22 +0100

    MINOR FIX: Set Loose JS Interop In Header Tests For ColorModeSwitcher

--- EventHighway.Portal.Web.Tests.Unit/Components/Layout/HeaderComponentTests.cs
+++ EventHighway.Portal.Web.Tests.Unit/Components/Layout/HeaderComponentTests.cs
@@ -8,5 +8,10 @@ namespace EventHighway.Portal.Web.Tests.Unit.Components.Layout
 {
     public partial class HeaderComponentTests : BunitContext
     {
+        public HeaderComponentTests()
+        {
+            // The header hosts ColorModeSwitcher, which calls JS interop on render.
+            JSInterop.Mode = JSRuntimeMode.Loose;
+        }
     }
 }
