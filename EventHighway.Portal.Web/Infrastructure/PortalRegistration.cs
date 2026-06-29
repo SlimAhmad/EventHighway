0f9ecef8fa0026375ad448064036ecd474e70285
Christo du Toit <christo@dutoit.co.uk>
2026-06-29 08:38:43 +0100

    PAGES: Add Event Participant Secrets Child Section To Detail Page

--- EventHighway.Portal.Web/Infrastructure/PortalRegistration.cs
+++ EventHighway.Portal.Web/Infrastructure/PortalRegistration.cs
@@ -11,6 +11,7 @@ using EventHighway.Portal.Web.Brokers.DateTimes;
 using EventHighway.Portal.Web.Brokers.EventHighways;
 using EventHighway.Portal.Web.Brokers.Loggings;
 using EventHighway.Portal.Web.Services.Views.EventParticipants;
+using EventHighway.Portal.Web.Services.Views.EventParticipantSecrets;
 using EventHighway.Portal.Web.Services.Views.HealthDashboards;
 
 namespace EventHighway.Portal.Web.Infrastructure
@@ -40,6 +41,9 @@ namespace EventHighway.Portal.Web.Infrastructure
         {
             services.AddTransient<IHealthViewService, HealthViewService>();
             services.AddTransient<IEventParticipantsViewService, EventParticipantsViewService>();
+            services.AddTransient<
+                IEventParticipantSecretsViewService,
+                EventParticipantSecretsViewService>();
 
             return services;
         }
