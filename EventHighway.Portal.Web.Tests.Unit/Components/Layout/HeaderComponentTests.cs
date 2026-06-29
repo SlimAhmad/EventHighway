af8f904ec7a75ca623f61a5eb37628b26207d1c4
Christo du Toit <christo@dutoit.co.uk>
2026-06-29 22:23:09 +0100

    COMPONENTS: Add Header Auth State Component Tests

---
+++ EventHighway.Portal.Web.Tests.Unit/Components/Layout/HeaderComponentTests.cs
@@ -0,0 +1,12 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Bunit;
+
+namespace EventHighway.Portal.Web.Tests.Unit.Components.Layout
+{
+    public partial class HeaderComponentTests : BunitContext
+    {
+    }
+}
