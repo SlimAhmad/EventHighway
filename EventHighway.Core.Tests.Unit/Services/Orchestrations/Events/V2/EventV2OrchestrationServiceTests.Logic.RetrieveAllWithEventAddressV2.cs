974b9b707ea6adc95b7220f2dcf20ed6acc32b8d
Christo du Toit <christo@dutoit.co.uk>
2026-06-30 13:57:47 +0100

    ORCHESTRATIONS: RetrieveAllEventV2sWithEventAddressV2 Failing Test

---
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/Events/V2/EventV2OrchestrationServiceTests.Logic.RetrieveAllWithEventAddressV2.cs
@@ -0,0 +1,48 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.Events.V2
+{
+    public partial class EventV2OrchestrationServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveAllEventV2sWithEventAddressV2Async()
+        {
+            // given
+            CancellationToken randomCancellationToken = TestContext.Current.CancellationToken;
+            IQueryable<EventV2> randomEventV2s = CreateRandomEventV2s();
+            IQueryable<EventV2> retrievedEventV2s = randomEventV2s;
+            IQueryable<EventV2> expectedEventV2s = randomEventV2s.DeepClone();
+
+            this.eventV2ProcessingServiceMock.Setup(service =>
+                service.RetrieveAllEventV2sWithEventAddressV2Async(randomCancellationToken))
+                    .ReturnsAsync(retrievedEventV2s);
+
+            // when
+            IQueryable<EventV2> actualEventV2s =
+                await this.eventV2OrchestrationService
+                    .RetrieveAllEventV2sWithEventAddressV2Async(randomCancellationToken);
+
+            // then
+            actualEventV2s.Should().BeEquivalentTo(expectedEventV2s);
+
+            this.eventV2ProcessingServiceMock.Verify(service =>
+                service.RetrieveAllEventV2sWithEventAddressV2Async(randomCancellationToken),
+                    Times.Once);
+
+            this.eventV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.eventAddressV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.eventCallV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
