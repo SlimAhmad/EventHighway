7ce7e959abbbb65dea3a8439f1e370f30aadd719
Christo du Toit <christo@dutoit.co.uk>
2026-06-30 13:47:41 +0100

    PROCESSINGS: RetrieveAllEventV2sWithEventAddressV2 Failing Test

---
+++ EventHighway.Core.Tests.Unit/Services/Processings/Events/V2/EventV2ProcessingServiceTests.Logic.RetrieveAllWithEventAddressV2.cs
@@ -0,0 +1,49 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.Events.V2;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Processings.Events.V2
+{
+    public partial class EventV2ProcessingServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveAllEventV2sWithEventAddressV2Async()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            IQueryable<EventV2> randomEventV2s = CreateRandomEventV2s();
+            IQueryable<EventV2> retrievedEventV2s = randomEventV2s;
+            IQueryable<EventV2> expectedEventV2s = randomEventV2s.DeepClone();
+
+            this.eventV2ServiceMock.Setup(service =>
+                service.RetrieveAllEventV2sWithEventAddressV2Async(randomCancellationToken))
+                    .ReturnsAsync(retrievedEventV2s);
+
+            // when
+            IQueryable<EventV2> actualEventV2s =
+                await this.eventV2ProcessingService
+                    .RetrieveAllEventV2sWithEventAddressV2Async(randomCancellationToken);
+
+            // then
+            actualEventV2s.Should().BeEquivalentTo(expectedEventV2s);
+
+            this.eventV2ServiceMock.Verify(service =>
+                service.RetrieveAllEventV2sWithEventAddressV2Async(randomCancellationToken),
+                    Times.Once);
+
+            this.eventV2ServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
