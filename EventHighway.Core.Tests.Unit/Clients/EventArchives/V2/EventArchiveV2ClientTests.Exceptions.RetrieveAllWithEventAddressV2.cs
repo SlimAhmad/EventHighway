dee789f2474a4005478acf2accbfab958cfaa385
Christo du Toit <christo@dutoit.co.uk>
2026-06-30 18:58:37 +0100

    CODE RUB: Code Cleanup

--- EventHighway.Core.Tests.Unit/Clients/EventArchives/V2/EventArchiveV2ClientTests.Exceptions.RetrieveAllWithEventAddressV2.cs
+++ EventHighway.Core.Tests.Unit/Clients/EventArchives/V2/EventArchiveV2ClientTests.Exceptions.RetrieveAllWithEventAddressV2.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
