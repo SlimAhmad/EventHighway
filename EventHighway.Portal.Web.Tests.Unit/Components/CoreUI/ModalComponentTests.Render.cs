16193b8f1f309718a59840a53a5edc90257e4494
Christo du Toit <christo@dutoit.co.uk>
2026-07-01 01:11:38 +0100

    COMPONENTS: Add Modal Size And Scrollable Render Tests

--- EventHighway.Portal.Web.Tests.Unit/Components/CoreUI/ModalComponentTests.Render.cs
+++ EventHighway.Portal.Web.Tests.Unit/Components/CoreUI/ModalComponentTests.Render.cs
@@ -42,6 +42,53 @@ namespace EventHighway.Portal.Web.Tests.Unit.Components.CoreUI
         }
 
         [Fact]
+        public void ShouldRenderDefaultDialogWithoutSizeOrScrollableClasses()
+        {
+            // given . when
+            IRenderedComponent<Modal> renderedModal =
+                Render<Modal>(parameters => parameters
+                    .Add(modal => modal.Visible, true));
+
+            // then
+            string dialogClass = renderedModal.Find("div.modal-dialog").GetAttribute("class");
+
+            dialogClass.Should().Contain("modal-dialog-centered");
+            dialogClass.Should().NotContain("modal-dialog-scrollable");
+            dialogClass.Should().NotContain("modal-lg");
+            dialogClass.Should().NotContain("modal-xl");
+        }
+
+        [Theory]
+        [InlineData("lg")]
+        [InlineData("xl")]
+        public void ShouldRenderSizeClassWhenSizeProvided(string size)
+        {
+            // given . when
+            IRenderedComponent<Modal> renderedModal =
+                Render<Modal>(parameters => parameters
+                    .Add(modal => modal.Visible, true)
+                    .Add(modal => modal.Size, size));
+
+            // then
+            renderedModal.Find("div.modal-dialog").GetAttribute("class")
+                .Should().Contain($"modal-{size}");
+        }
+
+        [Fact]
+        public void ShouldRenderScrollableClassWhenScrollableIsTrue()
+        {
+            // given . when
+            IRenderedComponent<Modal> renderedModal =
+                Render<Modal>(parameters => parameters
+                    .Add(modal => modal.Visible, true)
+                    .Add(modal => modal.Scrollable, true));
+
+            // then
+            renderedModal.Find("div.modal-dialog").GetAttribute("class")
+                .Should().Contain("modal-dialog-scrollable");
+        }
+
+        [Fact]
         public void ShouldInvokeOnCloseWhenCloseButtonClicked()
         {
             // given
