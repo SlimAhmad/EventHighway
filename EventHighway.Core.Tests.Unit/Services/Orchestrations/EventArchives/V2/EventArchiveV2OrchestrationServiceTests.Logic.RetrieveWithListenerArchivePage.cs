70fe9b47fbc324ccd57a3fb5beae7ed09d296951
Christo du Toit <christo@dutoit.co.uk>
2026-07-02 22:09:23 +0100

    ShouldRetrieveEventArchiveV2WithListenerEventArchiveV2sByIdWithPagingAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V2/EventArchiveV2OrchestrationServiceTests.Logic.RetrieveWithListenerArchivePage.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V2/EventArchiveV2OrchestrationServiceTests.Logic.RetrieveWithListenerArchivePage.cs
@@ -90,6 +90,89 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V2
 
         [Fact]
         public async Task
+            ShouldRetrieveEventArchiveV2WithListenerEventArchiveV2sByIdWithPagingAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            Guid inputEventArchiveId = GetRandomId();
+            DateTimeOffset baseCreatedDate = GetRandomDateTimeOffset();
+
+            var firstListenerEventArchiveV2 = new ListenerEventArchiveV2
+            {
+                Id = GetRandomId(),
+                EventArchiveV2Id = inputEventArchiveId,
+                EventAddressV2Id = GetRandomId(),
+                EventListenerV2Id = GetRandomId(),
+                CreatedDate = baseCreatedDate
+            };
+
+            var secondListenerEventArchiveV2 = new ListenerEventArchiveV2
+            {
+                Id = GetRandomId(),
+                EventArchiveV2Id = inputEventArchiveId,
+                EventAddressV2Id = GetRandomId(),
+                EventListenerV2Id = GetRandomId(),
+                CreatedDate = baseCreatedDate.AddDays(1)
+            };
+
+            var thirdListenerEventArchiveV2 = new ListenerEventArchiveV2
+            {
+                Id = GetRandomId(),
+                EventArchiveV2Id = inputEventArchiveId,
+                EventAddressV2Id = GetRandomId(),
+                EventListenerV2Id = GetRandomId(),
+                CreatedDate = baseCreatedDate.AddDays(2)
+            };
+
+            IQueryable<ListenerEventArchiveV2> storedListenerEventArchiveV2s =
+                new List<ListenerEventArchiveV2>
+                {
+                    thirdListenerEventArchiveV2,
+                    firstListenerEventArchiveV2,
+                    secondListenerEventArchiveV2
+                }
+                    .AsQueryable();
+
+            var expectedEventArchiveV2 = new EventArchiveV2
+            {
+                Id = inputEventArchiveId,
+                ListenerEventArchiveV2s =
+                    new List<ListenerEventArchiveV2> { secondListenerEventArchiveV2 }
+            };
+
+            this.listenerEventArchiveV2ProcessingServiceMock.Setup(service =>
+                service.RetrieveAllListenerEventArchiveV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(storedListenerEventArchiveV2s);
+
+            // when
+            EventArchiveV2 actualEventArchiveV2 =
+                await this.eventArchiveV2OrchestrationService
+                    .RetrieveEventArchiveV2WithListenerEventArchiveV2sByIdAsync(
+                        eventArchiveId: inputEventArchiveId,
+                        eventListenerIds: null,
+                        startDate: null,
+                        endDate: null,
+                        skip: 1,
+                        take: 1,
+                        randomCancellationToken);
+
+            // then
+            actualEventArchiveV2.Should()
+                .BeEquivalentTo(expectedEventArchiveV2);
+
+            this.listenerEventArchiveV2ProcessingServiceMock.Verify(service =>
+                service.RetrieveAllListenerEventArchiveV2sAsync(randomCancellationToken),
+                    Times.Once);
+
+            this.listenerEventArchiveV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.eventArchiveV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task
             ShouldRetrieveEventArchiveV2WithListenerEventArchiveV2sByIdFilteredByEndDateAsync()
         {
             // given
