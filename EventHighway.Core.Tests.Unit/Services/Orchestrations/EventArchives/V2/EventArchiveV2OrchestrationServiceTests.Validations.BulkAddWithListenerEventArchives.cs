6d222f566acfe0dce09a961355fa7370e7743ec2
Christo du Toit <christo@dutoit.co.uk>
2026-07-02 11:29:33 +0100

    ShouldThrowValidationExceptionOnBulkAddWithListenerEventArchivesIfEventArchiveV2sIsNullAndLogItAsync -> FAIL

---
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V2/EventArchiveV2OrchestrationServiceTests.Validations.BulkAddWithListenerEventArchives.cs
@@ -0,0 +1,69 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventHighway.Core.Models.Services.Foundations.EventsArchives.V2;
+using EventHighway.Core.Models.Services.Foundations.ListenerEventArchives.V2;
+using EventHighway.Core.Models.Services.Orchestrations.EventArchives.V2.Exceptions;
+using FluentAssertions;
+using Moq;
+
+namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V2
+{
+    public partial class EventArchiveV2OrchestrationServiceTests
+    {
+        [Fact]
+        public async Task
+            ShouldThrowValidationExceptionOnBulkAddWithListenerEventArchivesIfEventArchiveV2sIsNullAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            List<EventArchiveV2> nullEventArchiveV2s = null;
+
+            var nullEventArchiveV2sOrchestrationException =
+                new NullEventArchiveV2sOrchestrationException(
+                    message: "Event archives are null.");
+
+            var expectedEventArchiveV2OrchestrationValidationException =
+                new EventArchiveV2OrchestrationValidationException(
+                    message: "Event archive validation error occurred, fix the errors and try again.",
+                    innerException: nullEventArchiveV2sOrchestrationException);
+
+            // when
+            ValueTask<IEnumerable<EventArchiveV2>> bulkAddEventArchiveV2sWithListenerEventArchiveV2sTask =
+                this.eventArchiveV2OrchestrationService
+                    .BulkAddEventArchiveV2sWithListenerEventArchiveV2sAsync(
+                        nullEventArchiveV2s,
+                        randomCancellationToken);
+
+            EventArchiveV2OrchestrationValidationException
+                actualEventArchiveV2OrchestrationValidationException =
+                    await Assert.ThrowsAsync<EventArchiveV2OrchestrationValidationException>(
+                        bulkAddEventArchiveV2sWithListenerEventArchiveV2sTask.AsTask);
+
+            // then
+            actualEventArchiveV2OrchestrationValidationException.Should().BeEquivalentTo(
+                expectedEventArchiveV2OrchestrationValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedEventArchiveV2OrchestrationValidationException))),
+                        Times.Once);
+
+            this.listenerEventArchiveV2ProcessingServiceMock.Verify(service =>
+                service.BulkAddListenerEventArchiveV2sAsync(
+                    It.IsAny<IEnumerable<ListenerEventArchiveV2>>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.eventArchiveV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.listenerEventArchiveV2ProcessingServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
