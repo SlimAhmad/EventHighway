68ecc83a8811ae5f38a001d7e5d074be95308834
Christo du Toit <christo@dutoit.co.uk>
2026-07-02 15:52:31 +0100

    ShouldRetrieveBatchOfEventArchiveV2sMatchingByEventListenerIdsAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V2/EventArchiveV2OrchestrationServiceTests.Logic.RetrieveBatchMatching.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/EventArchives/V2/EventArchiveV2OrchestrationServiceTests.Logic.RetrieveBatchMatching.cs
@@ -109,5 +109,102 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.EventArchives.V2
             this.eventArchiveV2ProcessingServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldRetrieveBatchOfEventArchiveV2sMatchingByEventListenerIdsAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            Guid inputEventListenerId = GetRandomId();
+
+            Guid matchingEventArchiveId = GetRandomId();
+            Guid otherEventArchiveId = GetRandomId();
+
+            var matchingEventArchiveV2 = new EventArchiveV2
+            {
+                Id = matchingEventArchiveId,
+                CreatedDate = GetRandomDateTimeOffset()
+            };
+
+            var otherEventArchiveV2 = new EventArchiveV2
+            {
+                Id = otherEventArchiveId,
+                CreatedDate = GetRandomDateTimeOffset()
+            };
+
+            var matchingListenerEventArchiveV2 = new ListenerEventArchiveV2
+            {
+                Id = GetRandomId(),
+                EventArchiveV2Id = matchingEventArchiveId,
+                EventAddressV2Id = GetRandomId(),
+                EventListenerV2Id = inputEventListenerId,
+                CreatedDate = GetRandomDateTimeOffset()
+            };
+
+            var otherListenerEventArchiveV2 = new ListenerEventArchiveV2
+            {
+                Id = GetRandomId(),
+                EventArchiveV2Id = otherEventArchiveId,
+                EventAddressV2Id = GetRandomId(),
+                EventListenerV2Id = GetRandomId(),
+                CreatedDate = GetRandomDateTimeOffset()
+            };
+
+            IQueryable<EventArchiveV2> storedEventArchiveV2s =
+                new List<EventArchiveV2> { matchingEventArchiveV2, otherEventArchiveV2 }
+                    .AsQueryable();
+
+            IQueryable<ListenerEventArchiveV2> storedListenerEventArchiveV2s =
+                new List<ListenerEventArchiveV2>
+                {
+                    matchingListenerEventArchiveV2,
+                    otherListenerEventArchiveV2
+                }
+                    .AsQueryable();
+
+            List<Guid> inputEventListenerIds =
+                new List<Guid> { inputEventListenerId };
+
+            List<EventArchiveV2> expectedEventArchiveV2s =
+                new List<EventArchiveV2> { matchingEventArchiveV2 };
+
+            this.listenerEventArchiveV2ProcessingServiceMock.Setup(service =>
+                service.RetrieveAllListenerEventArchiveV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(storedListenerEventArchiveV2s);
+
+            this.eventArchiveV2ProcessingServiceMock.Setup(service =>
+                service.RetrieveAllEventArchiveV2sAsync(randomCancellationToken))
+                    .ReturnsAsync(storedEventArchiveV2s);
+
+            // when
+            IEnumerable<EventArchiveV2> actualEventArchiveV2s =
+                await this.eventArchiveV2OrchestrationService
+                    .RetrieveBatchOfEventArchiveV2sMatchingAsync(
+                        eventAddressId: null,
+                        eventListenerIds: inputEventListenerIds,
+                        startDate: null,
+                        endDate: null,
+                        skip: 0,
+                        take: 10,
+                        randomCancellationToken);
+
+            // then
+            actualEventArchiveV2s.Should()
+                .BeEquivalentTo(expectedEventArchiveV2s);
+
+            this.listenerEventArchiveV2ProcessingServiceMock.Verify(service =>
+                service.RetrieveAllListenerEventArchiveV2sAsync(randomCancellationToken),
+                    Times.Once);
+
+            this.eventArchiveV2ProcessingServiceMock.Verify(service =>
+                service.RetrieveAllEventArchiveV2sAsync(randomCancellationToken),
+                    Times.Once);
+
+            this.listenerEventArchiveV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.eventArchiveV2ProcessingServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
