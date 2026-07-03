a2bb55d5236448d5a2a04e146b2e7ab5a7f07e0b
Christo du Toit <christo@dutoit.co.uk>
2026-07-03 18:59:36 +0100

    ShouldSplitLoopDetectionSummaryV2ByParticipantForWindowAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Orchestrations/LoopDetections/V2/LoopDetectionV2OrchestrationServiceTests.Logic.RetrieveLoopDetectionSummary.cs
+++ EventHighway.Core.Tests.Unit/Services/Orchestrations/LoopDetections/V2/LoopDetectionV2OrchestrationServiceTests.Logic.RetrieveLoopDetectionSummary.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using EventHighway.Core.Models.Coordinations.HealthChecks.V2;
+using EventHighway.Core.Models.Services.Foundations.EventParticipants.V2;
 using EventHighway.Core.Models.Services.Foundations.Events.V2;
 using EventHighway.Core.Models.Services.Foundations.EventsArchives.V2;
 using FluentAssertions;
@@ -90,6 +91,119 @@ namespace EventHighway.Core.Tests.Unit.Services.Orchestrations.LoopDetections.V2
             detailB.ArchivedQuarantined.Should().Be(0);
             detailB.InWindow.Should().Be(1);
             detailB.MostRecentDetection.Should().Be(windowStart.AddHours(3));
+
+            VerifyLoopDetectionFoundationMocksOnce(randomCancellationToken);
+        }
+
+        [Fact]
+        public async Task ShouldSplitLoopDetectionSummaryV2ByParticipantForWindowAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            var windowStart = new DateTimeOffset(2026, 6, 24, 0, 0, 0, TimeSpan.Zero);
+            Guid addressId = Guid.NewGuid();
+            string addressName = GetRandomString();
+
+            Guid participantAId = Guid.NewGuid();
+            Guid participantBId = Guid.NewGuid();
+            string participantAName = GetRandomString();
+            string participantBName = GetRandomString();
+
+            EventParticipantV2 participantA = CreateEventParticipantV2(
+                participantAId, participantAName, GetRandomString(), GetRandomString(), true);
+
+            EventParticipantV2 participantB = CreateEventParticipantV2(
+                participantBId, participantBName, GetRandomString(), GetRandomString(), true);
+
+            var addresses = new[]
+            {
+                CreateEventAddressV2(addressId, addressName, GetRandomString())
+            }.AsQueryable();
+
+            var events = new[]
+            {
+                CreateEventV2ForParticipant(Guid.NewGuid(), addressId, participantAId, participantA,
+                    windowStart.AddHours(1), "a1", EventStatusV2.Quarantined),
+                CreateEventV2ForParticipant(Guid.NewGuid(), addressId, participantAId, participantA,
+                    windowStart.AddHours(2), "a2", EventStatusV2.Quarantined),
+                CreateEventV2ForParticipant(Guid.NewGuid(), addressId, participantBId, participantB,
+                    windowStart.AddHours(3), "b1", EventStatusV2.Quarantined)
+            }.AsQueryable();
+
+            var emptyArchivedEvents = Array.Empty<EventArchiveV2>().AsQueryable();
+
+            SetupLoopDetectionFoundationMocks(
+                randomCancellationToken, addresses, events, emptyArchivedEvents);
+
+            // when
+            LoopDetectionSummaryV2 actualSummary =
+                await this.loopDetectionV2OrchestrationService
+                    .RetrieveLoopDetectionSummaryV2Async(
+                        TrafficPeriodV2.Day, windowStart, randomCancellationToken);
+
+            // then
+            actualSummary.ByAddress.Should().HaveCount(2);
+
+            LoopDetailV2 detailA =
+                actualSummary.ByAddress.Single(d => d.EventParticipantV2Id == participantAId);
+
+            detailA.EventAddressV2Id.Should().Be(addressId);
+            detailA.EventAddressV2Name.Should().Be(addressName);
+            detailA.EventParticipantV2Name.Should().Be(participantAName);
+            detailA.ActiveQuarantined.Should().Be(2);
+            detailA.ArchivedQuarantined.Should().Be(0);
+            detailA.InWindow.Should().Be(2);
+            detailA.MostRecentDetection.Should().Be(windowStart.AddHours(2));
+
+            LoopDetailV2 detailB =
+                actualSummary.ByAddress.Single(d => d.EventParticipantV2Id == participantBId);
+
+            detailB.EventParticipantV2Name.Should().Be(participantBName);
+            detailB.ActiveQuarantined.Should().Be(1);
+            detailB.InWindow.Should().Be(1);
+            detailB.MostRecentDetection.Should().Be(windowStart.AddHours(3));
+
+            VerifyLoopDetectionFoundationMocksOnce(randomCancellationToken);
+        }
+
+        [Fact]
+        public async Task ShouldReturnEmptyByAddressLoopDetectionSummaryV2WhenNoQuarantineInWindowAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            var windowStart = new DateTimeOffset(2026, 6, 24, 0, 0, 0, TimeSpan.Zero);
+            Guid addressAId = Guid.NewGuid();
+
+            var addresses = new[]
+            {
+                CreateEventAddressV2(addressAId, GetRandomString(), GetRandomString())
+            }.AsQueryable();
+
+            var events = new[]
+            {
+                CreateEventV2ForAddress(addressAId, windowStart.AddHours(1), "h1", 5, EventStatusV2.Active)
+            }.AsQueryable();
+
+            var emptyArchivedEvents = Array.Empty<EventArchiveV2>().AsQueryable();
+
+            SetupLoopDetectionFoundationMocks(
+                randomCancellationToken, addresses, events, emptyArchivedEvents);
+
+            // when
+            LoopDetectionSummaryV2 actualSummary =
+                await this.loopDetectionV2OrchestrationService
+                    .RetrieveLoopDetectionSummaryV2Async(
+                        TrafficPeriodV2.Day, windowStart, randomCancellationToken);
+
+            // then
+            actualSummary.TotalActiveQuarantined.Should().Be(0);
+            actualSummary.TotalArchivedQuarantined.Should().Be(0);
+            actualSummary.TotalInWindow.Should().Be(0);
+            actualSummary.ByAddress.Should().BeEmpty();
 
             VerifyLoopDetectionFoundationMocksOnce(randomCancellationToken);
         }
