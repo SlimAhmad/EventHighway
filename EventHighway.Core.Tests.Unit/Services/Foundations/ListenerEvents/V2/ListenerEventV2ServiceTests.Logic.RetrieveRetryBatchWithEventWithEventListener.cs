56908b66d08a8c599c04044ce9b6c263f5a7dc41
Christo du Toit <christo@dutoit.co.uk>
2026-07-04 18:25:55 +0100

    ShouldRetrieveBatchOfEligibleRetryListenerEventV2sWithNavigationsOldestFirstAndTakeAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Logic.RetrieveRetryBatchWithEventWithEventListener.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Logic.RetrieveRetryBatchWithEventWithEventListener.cs
@@ -165,5 +165,71 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.ListenerEvents.V2
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldRetrieveBatchOfEligibleRetryListenerEventV2sWithNavigationsOldestFirstAndTakeAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            int take = 2;
+            DateTimeOffset now = GetRandomDateTimeOffset();
+
+            List<ListenerEventV2> randomListenerEventV2List =
+                CreateRandomListenerEventV2s().ToList();
+
+            for (int index = 0; index < randomListenerEventV2List.Count; index++)
+            {
+                ListenerEventV2 listenerEventV2 = randomListenerEventV2List[index];
+                listenerEventV2.Status = ListenerEventStatusV2.Error;
+                listenerEventV2.RemainingRetryAttempts = GetRandomNumber();
+                listenerEventV2.NextRetryAttemptNotBefore = null;
+                listenerEventV2.CreatedDate = now.AddMinutes(-index);
+            }
+
+            IQueryable<ListenerEventV2> randomListenerEventV2s =
+                randomListenerEventV2List.AsQueryable();
+
+            IEnumerable<ListenerEventV2> expectedListenerEventV2s =
+                randomListenerEventV2s
+                    .OrderBy(listenerEventV2 => listenerEventV2.CreatedDate)
+                    .Take(take)
+                    .DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllListenerEventV2sWithEventV2WithEventListenerV2Async(
+                    randomCancellationToken))
+                        .ReturnsAsync(randomListenerEventV2s);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(now);
+
+            // when
+            IEnumerable<ListenerEventV2> actualListenerEventV2s =
+                await this.listenerEventV2Service
+                    .RetrieveRetryBatchListenerEventV2sWithEventWithEventListenerAsync(
+                        take,
+                        randomCancellationToken);
+
+            // then
+            actualListenerEventV2s.Should().BeEquivalentTo(
+                expectedListenerEventV2s,
+                options => options.WithStrictOrdering());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllListenerEventV2sWithEventV2WithEventListenerV2Async(
+                    randomCancellationToken),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
