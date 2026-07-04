9285abb6160914a913229c7bdca09b3dea31fcc7
Christo du Toit <christo@dutoit.co.uk>
2026-07-04 18:23:18 +0100

    ShouldRetrieveOnlyEligibleRetryListenerEventV2sExcludingIneligibleAsync -> PASS

--- EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Logic.RetrieveRetryBatchWithEventWithEventListener.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Logic.RetrieveRetryBatchWithEventWithEventListener.cs
@@ -80,5 +80,90 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.ListenerEvents.V2
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldRetrieveOnlyEligibleRetryListenerEventV2sExcludingIneligibleAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            int take = 0;
+            DateTimeOffset now = GetRandomDateTimeOffset();
+
+            ListenerEventV2 eligibleWithNullGate = CreateRandomListenerEventV2();
+            eligibleWithNullGate.Status = ListenerEventStatusV2.Error;
+            eligibleWithNullGate.RemainingRetryAttempts = GetRandomNumber();
+            eligibleWithNullGate.NextRetryAttemptNotBefore = null;
+            eligibleWithNullGate.CreatedDate = now.AddMinutes(-4);
+
+            ListenerEventV2 eligibleWithPastGate = CreateRandomListenerEventV2();
+            eligibleWithPastGate.Status = ListenerEventStatusV2.Error;
+            eligibleWithPastGate.RemainingRetryAttempts = GetRandomNumber();
+            eligibleWithPastGate.NextRetryAttemptNotBefore = now.AddMinutes(-1);
+            eligibleWithPastGate.CreatedDate = now.AddMinutes(-3);
+
+            ListenerEventV2 ineligibleBecauseSuccess = CreateRandomListenerEventV2();
+            ineligibleBecauseSuccess.Status = ListenerEventStatusV2.Success;
+            ineligibleBecauseSuccess.RemainingRetryAttempts = GetRandomNumber();
+            ineligibleBecauseSuccess.NextRetryAttemptNotBefore = null;
+
+            ListenerEventV2 ineligibleBecauseNoRetries = CreateRandomListenerEventV2();
+            ineligibleBecauseNoRetries.Status = ListenerEventStatusV2.Error;
+            ineligibleBecauseNoRetries.RemainingRetryAttempts = 0;
+            ineligibleBecauseNoRetries.NextRetryAttemptNotBefore = null;
+
+            ListenerEventV2 ineligibleBecauseFutureGate = CreateRandomListenerEventV2();
+            ineligibleBecauseFutureGate.Status = ListenerEventStatusV2.Error;
+            ineligibleBecauseFutureGate.RemainingRetryAttempts = GetRandomNumber();
+            ineligibleBecauseFutureGate.NextRetryAttemptNotBefore = now.AddMinutes(5);
+
+            IQueryable<ListenerEventV2> randomListenerEventV2s = new List<ListenerEventV2>
+            {
+                ineligibleBecauseFutureGate,
+                eligibleWithPastGate,
+                ineligibleBecauseSuccess,
+                eligibleWithNullGate,
+                ineligibleBecauseNoRetries
+            }.AsQueryable();
+
+            IEnumerable<ListenerEventV2> expectedListenerEventV2s =
+                new List<ListenerEventV2> { eligibleWithNullGate, eligibleWithPastGate }
+                    .DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllListenerEventV2sWithEventV2WithEventListenerV2Async(
+                    randomCancellationToken))
+                        .ReturnsAsync(randomListenerEventV2s);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetDateTimeOffsetAsync())
+                    .ReturnsAsync(now);
+
+            // when
+            IEnumerable<ListenerEventV2> actualListenerEventV2s =
+                await this.listenerEventV2Service
+                    .RetrieveRetryBatchListenerEventV2sWithEventWithEventListenerAsync(
+                        take,
+                        randomCancellationToken);
+
+            // then
+            actualListenerEventV2s.Should().BeEquivalentTo(
+                expectedListenerEventV2s,
+                options => options.WithStrictOrdering());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllListenerEventV2sWithEventV2WithEventListenerV2Async(
+                    randomCancellationToken),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
