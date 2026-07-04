d1b1b716d0237a711ef914a0b0da7e49a4238c91
Christo du Toit <christo@dutoit.co.uk>
2026-07-04 19:36:56 +0100

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfListenerEventV2IsNotFoundAndLogItAsync -> FAIL

--- EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Validations.RetrieveById.cs
+++ EventHighway.Core.Tests.Unit/Services/Foundations/ListenerEvents/V2/ListenerEventV2ServiceTests.Validations.RetrieveById.cs
@@ -65,5 +65,60 @@ namespace EventHighway.Core.Tests.Unit.Services.Foundations.ListenerEvents.V2
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRetrieveByIdIfListenerEventV2IsNotFoundAndLogItAsync()
+        {
+            // given
+            CancellationToken randomCancellationToken =
+                TestContext.Current.CancellationToken;
+
+            Guid nonExistingListenerEventV2Id = GetRandomId();
+            ListenerEventV2 nullListenerEventV2 = null;
+
+            var notFoundListenerEventV2Exception =
+                new NotFoundListenerEventV2Exception(
+                    message: $"Could not find listener event with id: {nonExistingListenerEventV2Id}.");
+
+            var expectedListenerEventV2ValidationException =
+                new ListenerEventV2ValidationException(
+                    message: "Listener event validation error occurred, fix the errors and try again.",
+                    innerException: notFoundListenerEventV2Exception);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectListenerEventV2ByIdAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<CancellationToken>()))
+                        .ReturnsAsync(nullListenerEventV2);
+
+            // when
+            ValueTask<ListenerEventV2> retrieveListenerEventV2ByIdTask =
+                this.listenerEventV2Service.RetrieveListenerEventV2ByIdAsync(
+                    nonExistingListenerEventV2Id,
+                    randomCancellationToken);
+
+            ListenerEventV2ValidationException actualListenerEventV2ValidationException =
+                await Assert.ThrowsAsync<ListenerEventV2ValidationException>(
+                    retrieveListenerEventV2ByIdTask.AsTask);
+
+            // then
+            actualListenerEventV2ValidationException.Should()
+                .BeEquivalentTo(expectedListenerEventV2ValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectListenerEventV2ByIdAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<CancellationToken>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedListenerEventV2ValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
